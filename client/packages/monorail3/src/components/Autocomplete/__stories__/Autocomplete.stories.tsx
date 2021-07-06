d3ac6ede07a1e5b557f3ef03c295a29e39032938
Andy White <andywhite441@gmail.com>
2021-07-06 11:29:45 -0600

    Autocomplete stories (partial)

--- client/packages/monorail3/src/components/Autocomplete/__stories__/Autocomplete.stories.tsx
+++ client/packages/monorail3/src/components/Autocomplete/__stories__/Autocomplete.stories.tsx
@@ -4,7 +4,14 @@ import { Autocomplete, AutocompleteProps } from '../Autocomplete'
 import { story } from '../../../__tests__/helpers/storybook'
 import { defaultStoryMeta } from './Autocomplete.stories.gen'
 import { TextField } from '../../TextField/TextField'
-import { Movie, movies } from '../../../__tests__/helpers/testData'
+import {
+  countries,
+  countryToFlag,
+  Movie,
+  movies,
+} from '../../../__tests__/helpers/testData'
+import { Box } from '../../Box/Box'
+import { Stack } from '../../Stack/Stack'
 /**
  * Metadata for Autocomplete stories - update/extend as needed
  * This is intended to be exported as story-level metadata from the main .stories.tsx file, like:
@@ -44,4 +51,77 @@ const Template = story<MovieAutocompleteProps>(
 
 /** Default story for Autocomplete (edit/remove by hand if needed) */
 export const Default = story(Template)
-// TODO: add more stories below
+
+export const ComboBox = () => (
+  <MovieAutocomplete
+    disablePortal
+    id="combo-box-demo"
+    options={movies}
+    sx={{ width: 300 }}
+    renderInput={params => <TextField {...params} label="Movie" />}
+  />
+)
+
+export const FancySelectItems = () => (
+  <Autocomplete
+    id="country-select-demo"
+    sx={{ width: 300 }}
+    options={countries}
+    autoHighlight
+    getOptionLabel={option => option.label}
+    renderOption={(props, option) => (
+      <Box
+        component="li"
+        sx={{ fontSize: 15, '& > span': { mr: '10px', fontSize: 18 } }}
+        {...props}
+      >
+        <span>{countryToFlag(option.code)}</span>
+        {option.label} ({option.code}) +{option.phone}
+      </Box>
+    )}
+    renderInput={params => (
+      <TextField
+        {...params}
+        label="Choose a country"
+        inputProps={{
+          ...params.inputProps,
+          autoComplete: 'new-password', // disable autocomplete and autofill
+        }}
+      />
+    )}
+  />
+)
+
+export const FreeSolo = () => (
+  <>
+    <p>
+      Free solo mode allows arbitrary input text, in addition to suggestions
+    </p>
+    <p>As a default TextField</p>
+    <Stack spacing={2} sx={{ width: 300 }}>
+      <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={movies.map(option => option.label)}
+        renderInput={params => <TextField {...params} label="freeSolo" />}
+      />
+      <p>As a TextField with type=search</p>
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={movies.map(option => option.label)}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label="Search input"
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+            }}
+          />
+        )}
+      />
+    </Stack>
+  </>
+)
