1d4f25259a9e4e957223ec6e9be7123b627a99c9
Nick Brinser <nickbrinser@gmail.com>
2021-07-13 10:53:13 -0500

    feat(PS-20411): select initial basic story

--- client/packages/monorail3/src/components/Select/__stories__/Select.stories.tsx
+++ client/packages/monorail3/src/components/Select/__stories__/Select.stories.tsx
@@ -1,19 +1,57 @@
 // Edit this file to add new stories
 import React from 'react'
-import { Select, SelectProps } from '../Select'
 import { story } from '../../../__tests__/helpers/storybook'
+import { Select, SelectProps } from '../Select'
 import { defaultStoryMeta } from './Select.stories.gen'
-/**
- * Metadata for Select stories - update/extend as needed
- */
+
+import { Box } from '../../Box/Box'
+import { FormControl } from '../../FormControl/FormControl'
+import { InputLabel } from '../../InputLabel/InputLabel'
+import { MenuItem } from '../../MenuItem/MenuItem'
+
 export default { ...defaultStoryMeta }
-/**
- * Story template (edit/remove by hand if needed)
- *
- * Note: there should be at least one "Default" story that uses this template with the "story" function.
- * The Template and "story" function allow the story to be setup so that it works with the Controls addon and docgen
- */
-const Template = story<SelectProps>(args => <Select {...args} />, { args: {} })
+
+const Template = story<SelectProps>(
+  args => {
+    const [age, setAge] = React.useState('')
+
+    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+      setAge(event.target.value as string)
+    }
+
+    return (
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+            {...args}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    )
+  },
+  {
+    args: {},
+  },
+)
 /** Default story for Select (edit/remove by hand if needed) */
 export const Default = story(Template)
-// TODO: add more stories below
+
+export const BasicSelect = story(Template, {
+  parameters: {
+    docs: {
+      description: {
+        story: `Menus are positioned over their emitting elements such that the currently selected menu item appears on top of the emitting element.`,
+      },
+    },
+  },
+})
