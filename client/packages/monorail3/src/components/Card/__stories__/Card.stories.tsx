a46d08a3cd69c2d53712ac0b3b23a312b8709527
gsamaniego41 <gsamaniego41@gmail.com>
2021-07-20 13:19:01 -0400

    Default Card story controls

--- client/packages/monorail3/src/components/Card/__stories__/Card.stories.tsx
+++ client/packages/monorail3/src/components/Card/__stories__/Card.stories.tsx
@@ -37,13 +37,6 @@ export default { ...defaultStoryMeta }
  * Note: there should be at least one "Default" story that uses this template with the "story" function.
  * The Template and "story" function allow the story to be setup so that it works with the Controls addon and docgen
  */
-const Template = story<CardProps>(args => <Card {...args} />, { args: {} })
-/** Default story for Card (edit/remove by hand if needed) */
-// export const Default = story(Template)
-
-const IMG_URL_LIZARD =
-  'https://images.pexels.com/photos/735174/pexels-photo-735174.jpeg?cs=srgb&dl=pexels-sameera-madusanka-735174.jpg&fm=jpg'
-
 const bull = (
   <Box
     component="span"
@@ -53,33 +46,32 @@ const bull = (
   </Box>
 )
 
-export const Default = story<CardProps>(
-  () => {
-    return (
-      <Card sx={{ maxWidth: 275 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="div">
-            be{bull}nev{bull}o{bull}lent
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    )
-  },
+const Template = story<CardProps>(
+  args => (
+    <Card sx={{ maxWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  ),
   {
+    args: {},
     parameters: {
       docs: {
         description: {
@@ -90,6 +82,9 @@ export const Default = story<CardProps>(
     },
   },
 )
+
+/** Default story for Card (edit/remove by hand if needed) */
+export const Default = story(Template)
 
 export const OutlinedCard = story<CardProps>(
   () => {
