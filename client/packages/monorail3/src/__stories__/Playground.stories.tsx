d331bf1cbaa319855f548d549c5aeef2769a87b3
Andy White <andywhite441@gmail.com>
2021-07-22 08:52:27 -0600

    Generate index.js at build time

--- client/packages/monorail3/src/__stories__/Playground.stories.tsx
+++ client/packages/monorail3/src/__stories__/Playground.stories.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import * as m from '../index'
 import MenuIcon from '@material-ui/icons/Menu'
+import { Container } from '../components/Container/Container'
+import { AppBar } from '../components/AppBar/AppBar'
+import { Toolbar } from '../components/Toolbar/Toolbar'
+import { IconButton } from '../components/IconButton/IconButton'
+import { Typography } from '../components/Typography/Typography'
+import { Button } from '../components/Button/Button'
+import { Box } from '../components/Box/Box'
 
 export default {
   title: 'Theme/Full Page Demo',
 }
 
 // TODO: this needs to be built-out, but the intention is to have a somewhat realistic looking page
-// which can serve as a more built-out visual guide for theming. Also useful for testing the `import * as m`
-// for name collisions.
+// which can serve as a more built-out visual guide for theming.
 
 export const FullPageDemo = () => {
   return (
-    <m.Container>
-      <m.AppBar>
-        <m.Toolbar>
-          <m.IconButton
+    <Container>
+      <AppBar>
+        <Toolbar>
+          <IconButton
             size="large"
             edge="start"
             color="inherit"
@@ -23,17 +28,17 @@ export const FullPageDemo = () => {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
-          </m.IconButton>
-          <m.Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             My Cool New App
-          </m.Typography>
-          <m.Button color="inherit">Login</m.Button>
-        </m.Toolbar>
-      </m.AppBar>
-      <m.Box mt={8}>
-        <m.Typography variant="h1">This is the title</m.Typography>
-        <m.Button>Click me</m.Button>
-      </m.Box>
-    </m.Container>
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Box mt={8}>
+        <Typography variant="h1">This is the title</Typography>
+        <Button>Click me</Button>
+      </Box>
+    </Container>
   )
 }
