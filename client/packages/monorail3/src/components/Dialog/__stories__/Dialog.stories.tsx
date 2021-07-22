2a342b628b9f37328189e8fa073fd59ff0f95d5f
gsamaniego41 <gsamaniego41@gmail.com>
2021-07-22 14:25:04 -0400

    Dialog subcomponents stories

--- client/packages/monorail3/src/components/Dialog/__stories__/Dialog.stories.tsx
+++ client/packages/monorail3/src/components/Dialog/__stories__/Dialog.stories.tsx
@@ -912,14 +912,6 @@ export const ScrollDialog = story<DialogProps>(
               tabIndex={-1}
             >
               {longParagraph}
-              {/* {[...new Array(50)]
-                .map(
-                  () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                )
-                .join('\n')} */}
             </DialogContentText>
           </DialogContent>
           <DialogActions>
