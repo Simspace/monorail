903d3d85d8267dc669a72bd3739106b849822079
Gabriel Duquette <gabriel@simspace.com>
2021-07-23 14:03:18 -0400

    WIP

--- client/packages/monorail3/src/components/Drawer/__stories__/Drawer.stories.tsx
+++ client/packages/monorail3/src/components/Drawer/__stories__/Drawer.stories.tsx
@@ -3,6 +3,16 @@ import React from 'react'
 import { Drawer, DrawerProps } from '../Drawer'
 import { story } from '../../../__tests__/helpers/storybook'
 import { defaultStoryMeta } from './Drawer.stories.gen'
+import { Box } from '../../Box/Box'
+import { List } from '../../List/List'
+import { ListItem } from '../../ListItem/ListItem'
+import { ListItemText } from '../../ListItemText/ListItemText'
+import { Divider } from '../../Divider/Divider'
+import { Button } from '../../Button/Button'
+import InboxIcon from '@material-ui/icons/Inbox'
+import MailIcon from '@material-ui/icons/Mail'
+import { ListItemIcon } from '../../ListItemIcon/ListItemIcon'
+import { SwipeableDrawer } from '../../SwipeableDrawer/SwipeableDrawer'
 /**
  * Metadata for Drawer stories - update/extend as needed
  */
@@ -16,4 +26,87 @@ export default { ...defaultStoryMeta }
 const Template = story<DrawerProps>(args => <Drawer {...args} />, { args: {} })
 /** Default story for Drawer (edit/remove by hand if needed) */
 export const Default = story(Template)
-// TODO: add more stories below
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right'
+
+export const TemporaryDrawer = story<DrawerProps>(
+  () => {
+    const [state, setState] = React.useState({
+      top: false,
+      left: false,
+      bottom: false,
+      right: false,
+    })
+
+    const toggleDrawer = (anchor: Anchor, open: boolean) => (
+      event: React.KeyboardEvent | React.MouseEvent,
+    ) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return
+      }
+
+      setState({ ...state, [anchor]: open })
+    }
+
+    const list = (anchor: Anchor) => (
+      <Box
+        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    )
+
+    return (
+      <div>
+        {(['left', 'right', 'top', 'bottom'] as const).map(anchor => (
+          <React.Fragment key={anchor}>
+            <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+              {list(anchor)}
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
+    )
+  },
+  {
+    parameters: {
+      docs: {
+        description: {
+          story: `Temporary navigation drawers can toggle open or closed. Closed by default, the drawer opens temporarily above all other content until a section is selected. The Drawer can be cancelled by clicking the overlay or pressing the Esc key. It closes when an item is selected, handled by controlling the open prop.`,
+        },
+      },
+    },
+  },
+)
