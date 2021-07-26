48ac55a50e88f7432a4b1d140575d2811394c2fa
Paul Gray <pfbgray@gmail.com>
2021-07-26 10:49:43 -0400

    Moving Switch example story components to the stories file

--- client/packages/monorail3/src/components/Switch/__stories__/Switch.stories.tsx
+++ client/packages/monorail3/src/components/Switch/__stories__/Switch.stories.tsx
@@ -1,20 +1,22 @@
 // Edit this file to add new stories
+import { pink } from '@material-ui/core/colors'
+import { alpha, styled } from '@material-ui/core/styles'
 import React from 'react'
-import { Switch, SwitchProps } from '../Switch'
 import { story } from '../../../__tests__/helpers/storybook'
-import BasicSwitchesEx from './examples/BasicSwitches'
-import ColorSwitchesEx from './examples/ColorSwitches'
-import ControlledSwitchesEx from './examples/ControlledSwitches'
-import CustomizedSwitchesEx from './examples/CustomizedSwitches'
-import FormControlLabelPositionEx from './examples/FormControlLabelPosition'
-import SwitchesGroupEx from './examples/SwitchesGroup'
-import SwitchesSizeEx from './examples/SwitchesSize'
-import SwitchLabelsEx from './examples/SwitchLabels'
+import { FormControl } from '../../FormControl/FormControl'
+import { FormControlLabel } from '../../FormControlLabel/FormControlLabel'
+import { FormGroup } from '../../FormGroup/FormGroup'
+import { FormHelperText } from '../../FormHelperText/FormHelperText'
+import { FormLabel } from '../../FormLabel/FormLabel'
+import { Switch, SwitchProps } from '../Switch'
 import { defaultStoryMeta } from './Switch.stories.gen'
+
 /**
  * Metadata for Switch stories - update/extend as needed
  */
-export default { ...defaultStoryMeta }
+export default {
+  ...defaultStoryMeta,
+}
 /**
  * Story template (edit/remove by hand if needed)
  *
@@ -25,19 +27,151 @@ const Template = story<SwitchProps>(args => <Switch {...args} />, { args: {} })
 /** Default story for Switch (edit/remove by hand if needed) */
 export const Default = story(Template)
 
-export const BasicSwitches = story(() => <BasicSwitchesEx />)
+const label = { inputProps: { 'aria-label': 'Switch demo' } }
 
-export const ColorSwitches = story(() => <ColorSwitchesEx />)
+export const BasicSwitches = story(() => (
+  <div>
+    <Switch {...label} defaultChecked />
+    <Switch {...label} />
+    <Switch {...label} disabled defaultChecked />
+    <Switch {...label} disabled />
+  </div>
+))
 
-export const ControlledSwitches = story(() => <ControlledSwitchesEx />)
+const GreenSwitch = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-switchBase.Mui-checked': {
+    color: pink[600],
+    '&:hover': {
+      backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+    },
+  },
+  '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+    backgroundColor: pink[600],
+  },
+}))
 
-export const CustomizedSwitches = story(() => <CustomizedSwitchesEx />)
+export const ColorSwitches = story(() => (
+  <div>
+    <Switch {...label} defaultChecked />
+    <Switch {...label} defaultChecked color="secondary" />
+    <Switch {...label} defaultChecked color="warning" />
+    <Switch {...label} defaultChecked color="default" />
+    <GreenSwitch {...label} defaultChecked />
+  </div>
+))
 
-export const FormControlLabelPosition = story(() => <FormControlLabelPositionEx />)
+export const ControlledSwitches = story(() => {
+  const [checked, setChecked] = React.useState(true)
 
-export const SwitchLabels = story(() => <SwitchLabelsEx />)
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked)
+  }
 
-export const SwitchesGroup = story(() => <SwitchesGroupEx />)
+  return (
+    <Switch
+      checked={checked}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'controlled' }}
+    />
+  )
+})
 
-export const SwitchesSize = story(() => <SwitchesSizeEx />)
-        
+export const FormControlLabelPosition = story(() => (
+  <FormControl component="fieldset">
+    <FormLabel component="legend">Label placement</FormLabel>
+    <FormGroup aria-label="position" row>
+      <FormControlLabel
+        value="top"
+        control={<Switch color="primary" />}
+        label="Top"
+        labelPlacement="top"
+      />
+      <FormControlLabel
+        value="start"
+        control={<Switch color="primary" />}
+        label="Start"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        value="bottom"
+        control={<Switch color="primary" />}
+        label="Bottom"
+        labelPlacement="bottom"
+      />
+      <FormControlLabel
+        value="end"
+        control={<Switch color="primary" />}
+        label="End"
+        labelPlacement="end"
+      />
+    </FormGroup>
+  </FormControl>
+))
+
+export const SwitchLabels = story(() => (
+  <FormGroup>
+    <FormControlLabel control={<Switch defaultChecked />} label="Label" />
+    <FormControlLabel disabled control={<Switch />} label="Disabled" />
+  </FormGroup>
+))
+
+export const SwitchesGroup = story(() => {
+  const [state, setState] = React.useState({
+    gilad: true,
+    jason: false,
+    antoine: true,
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    })
+  }
+
+  return (
+    <FormControl component="fieldset" variant="standard">
+      <FormLabel component="legend">Assign responsibility</FormLabel>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={state.gilad}
+              onChange={handleChange}
+              name="gilad"
+            />
+          }
+          label="Gilad Gray"
+        />
+        <FormControlLabel
+          control={
+            <Switch
+              checked={state.jason}
+              onChange={handleChange}
+              name="jason"
+            />
+          }
+          label="Jason Killian"
+        />
+        <FormControlLabel
+          control={
+            <Switch
+              checked={state.antoine}
+              onChange={handleChange}
+              name="antoine"
+            />
+          }
+          label="Antoine Llorca"
+        />
+      </FormGroup>
+      <FormHelperText>Be careful</FormHelperText>
+    </FormControl>
+  )
+})
+
+export const SwitchesSize = story(() => (
+  <div>
+    <Switch {...label} defaultChecked size="small" />
+    <Switch {...label} defaultChecked />
+  </div>
+))
