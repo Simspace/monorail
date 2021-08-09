201664f449e0c52bb826ed1b5de74e343e0088bd
Adam Recvlohe <adam@simspace.com>
2021-08-09 15:23:01 -0500

    Add basic FilledInput and OutlinedInput stories

--- client/packages/monorail3/src/components/FilledInput/__stories__/FilledInput.stories.tsx
+++ client/packages/monorail3/src/components/FilledInput/__stories__/FilledInput.stories.tsx
@@ -3,19 +3,67 @@ import React from 'react'
 import { FilledInput, FilledInputProps } from '../FilledInput'
 import { story } from '../../../__tests__/helpers/storybook'
 import { defaultStoryMeta } from './FilledInput.stories.gen'
+import { FormControl } from '../../FormControl/FormControl'
+import { InputLabel } from '../../InputLabel/InputLabel'
+
 /**
  * Metadata for FilledInput stories - update/extend as needed
  */
-export default { ...defaultStoryMeta }
+export default { ...defaultStoryMeta, title: 'Inputs/Input/FilledInput' }
 /**
  * Story template (edit/remove by hand if needed)
  *
  * Note: there should be at least one "Default" story that uses this template with the "story" function.
  * The Template and "story" function allow the story to be setup so that it works with the Controls addon and docgen
  */
-const Template = story<FilledInputProps>(args => <FilledInput {...args} />, {
-  args: {},
-})
+const Template = story<FilledInputProps>(
+  args => (
+    <FormControl variant="filled">
+      <InputLabel htmlFor="filled">Label</InputLabel>
+      <FilledInput {...args} />
+    </FormControl>
+  ),
+  {
+    args: {
+      autoComplete: undefined,
+      autoFocus: false,
+      classes: undefined,
+      color: 'primary',
+      // components: {
+      //   Input: undefined,
+      //   Root: undefined,
+      // },
+      // Setting these as undefined set the existing components as undefined
+      // when in reality we want the defaults to render but that doesn't happen
+      componentsProps: undefined,
+      defaultValue: '',
+      disabled: false,
+      disableUnderline: false,
+      endAdornment: undefined,
+      error: false,
+      fullWidth: false,
+      hiddenLabel: false,
+      id: 'filled',
+      inputComponent: undefined,
+      inputProps: undefined,
+      inputRef: undefined,
+      margin: 'dense',
+      maxRows: undefined,
+      minRows: undefined,
+      multiline: false,
+      name: 'label',
+      onChange: undefined,
+      placeholder: 'Placeholder',
+      readOnly: false,
+      required: false,
+      rows: undefined,
+      startAdornment: undefined,
+      sx: undefined,
+      type: undefined,
+      value: undefined,
+    },
+  },
+)
 /** Default story for FilledInput (edit/remove by hand if needed) */
 export const Default = story(Template)
 // TODO: add more stories below
--- client/packages/monorail3/src/components/OutlinedInput/__stories__/OutlinedInput.stories.tsx
+++ client/packages/monorail3/src/components/OutlinedInput/__stories__/OutlinedInput.stories.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import { OutlinedInput, OutlinedInputProps } from '../OutlinedInput'
 import { story } from '../../../__tests__/helpers/storybook'
 import { defaultStoryMeta } from './OutlinedInput.stories.gen'
+import { FormControl } from '../../FormControl/FormControl'
+import { InputLabel } from '../../InputLabel/InputLabel'
 /**
  * Metadata for OutlinedInput stories - update/extend as needed
  */
-export default { ...defaultStoryMeta }
+export default { ...defaultStoryMeta, title: 'Inputs/Input/OutlinedInput' }
 /**
  * Story template (edit/remove by hand if needed)
  *
@@ -14,8 +16,51 @@ export default { ...defaultStoryMeta }
  * The Template and "story" function allow the story to be setup so that it works with the Controls addon and docgen
  */
 const Template = story<OutlinedInputProps>(
-  args => <OutlinedInput {...args} />,
-  { args: {} },
+  args => (
+    <FormControl>
+      <InputLabel htmlFor="outlined">Label</InputLabel>
+      <OutlinedInput id="outlined" label="Label" {...args} />
+    </FormControl>
+  ),
+  {
+    args: {
+      autoComplete: undefined,
+      autoFocus: false,
+      classes: undefined,
+      color: 'primary',
+      // components: {
+      //   Input: undefined,
+      //   Root: undefined,
+      // },
+      // Setting these as undefined set the existing components as undefined
+      // when in reality we want the defaults to render but that doesn't happen
+      defaultValue: '',
+      disabled: false,
+      endAdornment: undefined,
+      error: false,
+      fullWidth: false,
+      id: 'outlined',
+      inputComponent: undefined,
+      inputProps: undefined,
+      inputRef: undefined,
+      label: 'Label',
+      margin: 'dense',
+      maxRows: undefined,
+      minRows: undefined,
+      multiline: false,
+      name: 'label',
+      notched: undefined,
+      onChange: undefined,
+      placeholder: 'Placeholder',
+      readOnly: false,
+      required: false,
+      rows: undefined,
+      startAdornment: undefined,
+      sx: undefined,
+      type: undefined,
+      value: undefined,
+    },
+  },
 )
 /** Default story for OutlinedInput (edit/remove by hand if needed) */
 export const Default = story(Template)
