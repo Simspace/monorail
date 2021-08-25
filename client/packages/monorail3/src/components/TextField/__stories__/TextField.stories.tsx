dd13cbcfa6c38bd5e43648adf94834635cf400fe
Jacob Alford <jacob.alford@me.com>
2021-08-25 16:01:26 -0600

    Start TextField

--- client/packages/monorail3/src/components/TextField/__stories__/TextField.stories.tsx
+++ client/packages/monorail3/src/components/TextField/__stories__/TextField.stories.tsx
@@ -6,17 +6,9 @@ import { defaultStoryMeta } from './TextField.stories.gen'
 
 /**
  * Metadata for TextField stories - update/extend as needed
- * This is intended to be exported as story-level metadata from the main .stories.tsx file, like:
- * "export default { ...defaultStoryMeta } // Add/extend as needed
  */
 export default { ...defaultStoryMeta }
 
-/**
- * Story template (edit/remove by hand if needed)
- *
- * Note: there should be at least one "Default" story that uses this template with the "story" function.
- * The Template and "story" function allow the story to be setup so that it works with the Controls addon and docgen
- */
 const Template = story<TextFieldProps>(args => <TextField {...args} />, {
   args: { label: 'Text Field', inputProps: { 'aria-label': 'Text Field' } },
 })
