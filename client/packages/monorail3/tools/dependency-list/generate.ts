4aa169b66f8b16b3b23cfdae643ee1f6be7ebfff
Andy White <andywhite441@gmail.com>
2021-10-14 15:32:43 -0600

    Fix graph setup

--- client/packages/monorail3/tools/dependency-list/generate.ts
+++ client/packages/monorail3/tools/dependency-list/generate.ts
@@ -6,6 +6,7 @@ import * as R from 'fp-ts/lib/ReadonlyRecord'
 import * as A from 'fp-ts/lib/ReadonlyArray'
 import * as O from 'fp-ts/lib/Option'
 import * as str from 'fp-ts/lib/string'
+import * as S from 'fp-ts/lib/Set'
 import * as TE from 'fp-ts/lib/TaskEither'
 import * as T from 'fp-ts/lib/Task'
 import * as TS from 'topological-sort'
@@ -127,24 +128,47 @@ const topologicalSort = (
   // Normalize/clean all the dependency names
   input = normalizeInput(input)
 
-  // Convert the Record to Map (TopologicalSort wants a Map)
-  const inputMap = pipe(
+  // Create the graph nodes by using all the unique component names - the keys and values of the `input` record.
+  // The nodes are a map with a key for the node, and the node metadata value - in this case we don't care about the value, so just using {} for every node
+  const nodes: Map<string, {}> = pipe(
     input,
-    R.reduceWithIndex(
-      new Map<string, ReadonlyArray<string>>(),
-      (key, acc, value) => {
-        acc.set(key, value)
-        return acc
-      },
-    ),
+    R.reduceWithIndex(new Set<string>(), (component, acc, dependencies) => {
+      acc.add(component)
+      dependencies.forEach(dependency => acc.add(dependency))
+      return acc
+    }),
+    S.toArray(str.Ord),
+    A.map(component => [component, {}] as const),
+    tuples => new Map<string, {}>(tuples),
+  )
+
+  // Create the graph using all the nodes
+  const graph = new TS.TopologicalSort(nodes)
+
+  console.log(graph)
+
+  // Add the graph edges
+  // The input map has a component name as a key and the dependencies as the value array, so add edges from the key to all the value nodes
+  pipe(input, R.toReadonlyArray, a =>
+    a.forEach(([component, dependencies]) => {
+      dependencies.forEach(dependency => {
+        console.log('edge', component, dependency)
+        try {
+          graph.addEdge(component, dependency)
+        } catch (e) {
+           // There seems to be a double accounting for FilledInput -> InputBase
+          console.error('Ignoring bad edge:', component, dependency)
+        }
+      })
+    }),
   )
 
   // Do the sort - reverse the list to create the right order of depdencies
-  const ts = new TS.TopologicalSort(inputMap)
-  const sortedMap = ts.sort()
-  const sortedKeys = [...sortedMap.keys()].reverse()
+  const sortedGraph = graph.sort()
 
-  return sortedKeys
+  const sortedGraphKeys = [...sortedGraph.keys()].reverse()
+
+  return sortedGraphKeys
 }
 
 const mkdir = (
