e2bfdc50aa04e9416b38fc5daa049483490bf449
Andy White <andywhite441@gmail.com>
2022-01-10 16:43:37 -0700

    [PS-22582] add x-data-grid re-exports to fix imports in stories

--- client/packages/monorail3/src/components/DataGrid/DataGrid.tsx
+++ client/packages/monorail3/src/components/DataGrid/DataGrid.tsx
@@ -4,4 +4,44 @@ import {
   DataGridProps as _DataGridProps,
 } from './DataGrid'
 export * from './DataGrid.gen'
+
+// TODO: there are a lot of supporting types from x-data-grid that are just being re-exported here.
+// This is not strictly a problem, but raises some smells around how this is being handled.
+export {
+  GridActionsCellItem,
+  GridCellParams,
+  GridCellValue,
+  GridColDef,
+  GridColTypeDef,
+  GridColumnMenu,
+  GridColumnMenuContainer,
+  GridColumnMenuProps,
+  GridColumns,
+  GridFilterInputValueProps,
+  GridFilterItem,
+  GridFilterMenuItem,
+  GridFilterModel,
+  GridFilterOperator,
+  GridOverlay,
+  GridRenderCellParams,
+  GridRowData,
+  GridRowId,
+  GridRowModel,
+  GridRowParams,
+  GridRowsProp,
+  GridSortDirection,
+  GridSortModel,
+  GridToolbar,
+  GridToolbarColumnsButton,
+  GridToolbarContainer,
+  GridToolbarDensitySelector,
+  GridToolbarExport,
+  GridToolbarFilterButton,
+  GridValueFormatterParams,
+  GridValueGetterParams,
+  SortGridMenuItems,
+  getGridNumericColumnOperators,
+  useGridSlotComponentProps,
+} from '@mui/x-data-grid'
+
 // Add more functions/etc. below
