4ca57b5340b81f0d24ef85e8b9541f4096e55c43
gsamaniego41 <gsamaniego41@gmail.com>
2022-02-24 13:16:46 -0500

    Fix prettier error

--- client/packages/monorail3/src/components/SvgIcon/themeOverrides.ts
+++ client/packages/monorail3/src/components/SvgIcon/themeOverrides.ts
@@ -3,8 +3,8 @@ import { Components, Theme } from '@mui/material'
 export const MonorailSvgIconOverrides: Components<Theme>['MuiSvgIcon'] = {
   defaultProps: {},
   styleOverrides: {
-    root: ({theme}) => ({
-      color: theme.palette.grey[600]
-    })
+    root: ({ theme }) => ({
+      color: theme.palette.grey[600],
+    }),
   },
 }
