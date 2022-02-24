62bd7b8f33f9d7884f15654d06e52ad3f6899438
gsamaniego41 <gsamaniego41@gmail.com>
2022-02-24 18:30:46 -0500

    Remove color override

--- client/packages/monorail3/src/components/SvgIcon/themeOverrides.ts
+++ client/packages/monorail3/src/components/SvgIcon/themeOverrides.ts
@@ -2,9 +2,5 @@ import { Components, Theme } from '@mui/material'
 
 export const MonorailSvgIconOverrides: Components<Theme>['MuiSvgIcon'] = {
   defaultProps: {},
-  styleOverrides: {
-    root: ({ theme }) => ({
-      color: theme.palette.grey[600],
-    }),
-  },
+  styleOverrides: {},
 }
