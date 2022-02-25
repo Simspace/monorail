3ec5130f3cd83fd3a7a053429eeb82e6b9d4a81e
gsamaniego41 <gsamaniego41@gmail.com>
2022-02-25 10:20:34 -0500

    Remove Link overrides in story to display correct styles

--- client/packages/monorail3/src/components/Breadcrumbs/__stories__/Breadcrumbs.stories.tsx
+++ client/packages/monorail3/src/components/Breadcrumbs/__stories__/Breadcrumbs.stories.tsx
@@ -34,16 +34,8 @@ export default { title: 'Navigation/Breadcrumbs', component: Breadcrumbs }
 const Template = story<BreadcrumbsProps>(
   args => (
     <Breadcrumbs {...args}>
-      <Link underline="hover" color="inherit" href="/">
-        Material-UI
-      </Link>
-      <Link
-        underline="hover"
-        color="inherit"
-        href="/getting-started/installation/"
-      >
-        Core
-      </Link>
+      <Link href="/">Material-UI</Link>
+      <Link href="/getting-started/installation/">Core</Link>
       <Typography color="text.primary">Breadcrumbs</Typography>
     </Breadcrumbs>
   ),
@@ -59,18 +51,9 @@ export const LastItemInteractive = story<BreadcrumbsProps>(
   () => {
     return (
       <Breadcrumbs aria-label="breadcrumb">
-        <Link underline="hover" color="inherit" href="/">
-          Material-UI
-        </Link>
+        <Link href="/">Material-UI</Link>
+        <Link href="/getting-started/installation/">Core</Link>
         <Link
-          underline="hover"
-          color="inherit"
-          href="/getting-started/installation/"
-        >
-          Core
-        </Link>
-        <Link
-          underline="hover"
           color="text.primary"
           href="/components/breadcrumbs/"
           aria-current="page"
@@ -94,22 +77,10 @@ export const LastItemInteractive = story<BreadcrumbsProps>(
 export const CustomSeparator = story<BreadcrumbsProps>(
   () => {
     const breadcrumbs = [
-      <Link
-        underline="hover"
-        key="1"
-        color="inherit"
-        href="#link1"
-        onClick={action('Root Click')}
-      >
+      <Link key="1" href="#link1" onClick={action('Root Click')}>
         Material-UI
       </Link>,
-      <Link
-        underline="hover"
-        key="2"
-        color="inherit"
-        href="#link2"
-        onClick={action('Core Click')}
-      >
+      <Link key="2" href="#link2" onClick={action('Core Click')}>
         Core
       </Link>,
       <Typography key="3" color="text.primary">
@@ -150,19 +121,12 @@ export const BreadcrumbsWithIcons = story<BreadcrumbsProps>(
     return (
       <div role="presentation" onClick={action('Click')}>
         <Breadcrumbs aria-label="breadcrumb">
-          <Link
-            underline="hover"
-            sx={{ display: 'flex', alignItems: 'center' }}
-            color="inherit"
-            href="/"
-          >
+          <Link sx={{ display: 'flex', alignItems: 'center' }} href="/">
             <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
             Material-UI
           </Link>
           <Link
-            underline="hover"
             sx={{ display: 'flex', alignItems: 'center' }}
-            color="inherit"
             href="/getting-started/installation/"
           >
             <WhatshotIcon sx={{ mr: 0.5 }} fontSize="inherit" />
@@ -195,18 +159,10 @@ export const Collapsed = story<BreadcrumbsProps>(
     return (
       <div role="presentation" onClick={action('Click')}>
         <Breadcrumbs maxItems={2} aria-label="breadcrumb">
-          <Link underline="hover" color="inherit" href="#">
-            Home
-          </Link>
-          <Link underline="hover" color="inherit" href="#">
-            Catalog
-          </Link>
-          <Link underline="hover" color="inherit" href="#">
-            Accessories
-          </Link>
-          <Link underline="hover" color="inherit" href="#">
-            New Collection
-          </Link>
+          <Link href="#">Home</Link>
+          <Link href="#">Catalog</Link>
+          <Link href="#">Accessories</Link>
+          <Link href="#">New Collection</Link>
           <Typography color="text.primary">Belts</Typography>
         </Breadcrumbs>
       </div>
