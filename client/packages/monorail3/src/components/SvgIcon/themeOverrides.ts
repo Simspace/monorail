b60e6ba92ed66750cbca952368cec90b57cd6bce
gsamaniego41 <gsamaniego41@gmail.com>
2022-03-15 16:59:13 -0400

    define SvgIcon font sizes

--- client/packages/monorail3/src/components/SvgIcon/themeOverrides.ts
+++ client/packages/monorail3/src/components/SvgIcon/themeOverrides.ts
@@ -2,5 +2,12 @@ import { Components, Theme } from '@mui/material'
 
 export const MonorailSvgIconOverrides: Components<Theme>['MuiSvgIcon'] = {
   defaultProps: {},
-  styleOverrides: {},
+  styleOverrides: {
+    fontSizeSmall: {
+      fontSize: '1rem' // 16px
+    },
+    fontSizeLarge: {
+      fontSize: '2rem' // 32px
+    }
+  },
 }
