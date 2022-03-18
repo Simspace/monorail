2b53a15986e5c63ad9d5a7f192d99030b13699ed
gsamaniego41 <gsamaniego41@gmail.com>
2022-03-18 12:15:31 -0400

    implement pr feedback

--- client/packages/monorail3/src/components/IconButton/IconButton.tsx
+++ client/packages/monorail3/src/components/IconButton/IconButton.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import * as MUI from '@mui/material'
-import { styled } from '@mui/material/styles'
 
 export interface IconButtonProps extends MUI.IconButtonProps {
   variant?: 'contained' | 'outlined' | 'chromeless'
   shape?: 'circular' | 'rounded'
 }
 
-const StyledIconButton = styled(MUI.IconButton)``
-
-export const IconButton = (props: IconButtonProps) => (
-  <StyledIconButton variant="chromeless" shape="circular" {...props} />
+export const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
+  (props, ref) => (
+    <MUI.IconButton
+      ref={ref}
+      variant="chromeless"
+      shape="circular"
+      {...props}
+    />
+  ),
 )
