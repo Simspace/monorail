2c1a13a3cc708521bb3c93ee881590cebd90e329
gsamaniego41 <gsamaniego41@gmail.com>
2022-03-21 15:27:37 -0400

    update badge bgcolors

--- client/packages/monorail3/src/components/Badge/themeOverrides.ts
+++ client/packages/monorail3/src/components/Badge/themeOverrides.ts
@@ -7,5 +7,11 @@ export const MonorailBadgeOverrides: Components<Theme>['MuiBadge'] = {
       fontSize: theme.typography.pxToRem(14),
       lineHeight: theme.typography.pxToRem(18),
     }),
+    colorSecondary: ({ theme }) => ({
+      backgroundColor: theme.palette.secondary.light,
+    }),
+    colorWarning: ({ theme }) => ({
+      backgroundColor: theme.palette.warning.light,
+    }),
   },
 }
