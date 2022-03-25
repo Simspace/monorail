68817c45d0153773124b2fb6eb72163c4ce1d7e5
gsamaniego41 <gsamaniego41@gmail.com>
2022-03-25 12:18:07 -0400

    fix hover feedback example

--- client/packages/monorail3/src/components/Rating/__stories__/Rating.stories.tsx
+++ client/packages/monorail3/src/components/Rating/__stories__/Rating.stories.tsx
@@ -123,7 +123,7 @@ export const HoverFeedback = story<RatingProps>(
           onChangeActive={(_, newHover) => {
             setHover(newHover)
           }}
-          emptyIcon={<StarIcon />}
+          emptyIcon={<StarIcon fontSize="large" />}
         />
         {value !== null && (
           <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
