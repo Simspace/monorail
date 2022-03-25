ffaeafd0a71b4b8fa6955e262e5972bdd43cc0c0
gsamaniego41 <gsamaniego41@gmail.com>
2022-03-25 11:53:17 -0400

    update star sizes

--- client/packages/monorail3/src/components/Rating/themeOverrides.ts
+++ client/packages/monorail3/src/components/Rating/themeOverrides.ts
@@ -11,5 +11,14 @@ export const MonorailRatingOverrides: Components<Theme>['MuiRating'] = {
     iconFilled: ({ theme }) => ({
       color: theme.palette.rating,
     }),
+    sizeSmall: ({ theme }) => ({
+      fontSize: theme.typography.pxToRem(24),
+    }),
+    sizeMedium: ({ theme }) => ({
+      fontSize: theme.typography.pxToRem(32),
+    }),
+    sizeLarge: ({ theme }) => ({
+      fontSize: theme.typography.pxToRem(40),
+    }),
   },
 }
