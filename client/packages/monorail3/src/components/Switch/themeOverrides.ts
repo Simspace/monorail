41b3e701a14fde28121009d125142c5a97ff30d4
gsamaniego41 <gsamaniego41@gmail.com>
2022-04-15 13:50:55 -0400

    disabled, checked, and unchecked styling

--- client/packages/monorail3/src/components/Switch/themeOverrides.ts
+++ client/packages/monorail3/src/components/Switch/themeOverrides.ts
@@ -2,9 +2,9 @@ import { Components, Theme } from '@mui/material'
 
 const switchTokens = {
   track: {
-    idle: 'white',
     hover: 50,
     focused: 50,
+    disabled: 200,
   },
   border: {
     idle: 300,
@@ -17,16 +17,10 @@ const switchTokens = {
   icon: 600,
 
   checked: {
-    track: 600,
-    thumb: 'white',
-    bg: {
-      default: 600,
-      primary: 600,
-      secondary: 400,
-      info: 600,
-      error: 500,
-      warning: 400,
-      success: 500,
+    track: { disabled: 300 },
+    thumb: {
+      hover: 50,
+      disabled: 50,
     },
   },
 } as const
@@ -34,41 +28,88 @@ const switchTokens = {
 export const MonorailSwitchOverrides: Components<Theme>['MuiSwitch'] = {
   defaultProps: {},
   styleOverrides: {
-    root: {
+    root: ({ theme }) => ({
       padding: 0,
-    },
-    switchBase: ({ theme }) => ({
+      '&:hover': {
+        '& .MuiSwitch-track': {
+          borderColor: theme.palette.default[switchTokens.border.hover],
+          backgroundColor: theme.palette.default[switchTokens.track.hover],
+        },
+      },
+    }),
+    switchBase: ({ ownerState: { color = 'primary' }, theme }) => ({
+      // Focus styles not working
+      '&.Mui-focusVisible': {
+        '& > .MuiSwitch-track': {
+          borderColor: theme.palette.primary.focusRing.inner,
+        },
+      },
+
       padding: 4,
       '&:hover': {
         backgroundColor: 'transparent',
+        '&.Mui-checked .MuiSwitch-thumb': {
+          backgroundColor:
+            theme.palette[color][switchTokens.checked.thumb.hover],
+        },
+        '&.Mui-disabled': {
+          borderColor: 'transparent',
+        },
       },
       '&.Mui-checked': {
-        color: theme.palette.common.white,
         transform: 'translate(28px)',
+        '&:hover': {
+          backgroundColor: 'transparent',
+        },
+        '& + .MuiSwitch-track': {
+          backgroundColor: theme.palette[color].main,
+          opacity: 1,
+          borderColor: 'transparent',
+        },
+        '& .MuiSwitch-thumb': {
+          backgroundColor: theme.palette.common.white,
+        },
       },
-      '&.Mui-checked:hover': {
-        backgroundColor: 'transparent',
-      },
-      '&.Mui-checked + .MuiSwitch-track': {
-        opacity: 1,
+      '&.Mui-disabled': {
+        '& + .MuiSwitch-track': {
+          backgroundColor: theme.palette.default[switchTokens.track.disabled],
+          opacity: 1,
+          borderColor: 'transparent',
+        },
+        '& > .MuiSwitch-thumb': {
+          backgroundColor: theme.palette.common.white,
+        },
+        '&.Mui-checked': {
+          '& + .MuiSwitch-track': {
+            backgroundColor:
+              theme.palette.default[switchTokens.checked.track.disabled],
+          },
+          '& > .MuiSwitch-thumb': {
+            backgroundColor:
+              theme.palette.default[switchTokens.checked.thumb.disabled],
+          },
+        },
       },
     }),
+    // checked doesn't seem to work, so I used `.Mui-checked` inside `switchBase` -GS 4/15/22
     checked: {},
-    thumb: {
+    thumb: ({ theme }) => ({
+      backgroundColor: theme.palette.default[switchTokens.thumb.idle],
       boxShadow: 'none',
       width: 24,
       height: 24,
-    },
-    track: {
+    }),
+    track: ({ theme }) => ({
+      border: `2px solid ${theme.palette.default[switchTokens.border.idle]}`,
+      backgroundColor: theme.palette.common.white,
       borderRadius: 100,
-    },
+      opacity: 1,
+    }),
     sizeMedium: {
       height: 32,
       width: 60,
     },
-    disabled: ({ theme }) => ({
-      opacity: theme.palette.action.disabledOpacity,
-    }),
+    disabled: {},
     sizeSmall: {
       height: 24,
       width: 44,
