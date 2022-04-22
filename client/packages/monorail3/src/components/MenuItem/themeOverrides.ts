3b6a3676df7089e648886cde630654f4adde9cbb
gsamaniego41 <gsamaniego41@gmail.com>
2022-04-22 11:06:02 -0400

    menu item theme

--- client/packages/monorail3/src/components/MenuItem/themeOverrides.ts
+++ client/packages/monorail3/src/components/MenuItem/themeOverrides.ts
@@ -1,6 +1,21 @@
-import { Components, Theme } from '@mui/material'
+import { Components, menuItemClasses, Theme } from '@mui/material'
 
 export const MonorailMenuItemOverrides: Components<Theme>['MuiMenuItem'] = {
   defaultProps: {},
-  styleOverrides: {},
+  styleOverrides: {
+    root: ({ theme }) => ({
+      paddingTop: 12,
+      paddingBottom: 12,
+      '&:active': {
+        backgroundColor: theme.palette.primary[200],
+      },
+      [`&.${menuItemClasses.selected}`]: {
+        backgroundColor: theme.palette.action.selected,
+      },
+    }),
+    dense: {
+      paddingTop: 4,
+      paddingBottom: 4,
+    },
+  },
 }
