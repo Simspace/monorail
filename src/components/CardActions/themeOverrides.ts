417084abb9e6846594b8843e908efea55a024f25
Michael Martin <mlmartin13@gmail.com>
2022-04-26 13:34:05 -0600

    Improve size and space of card action footer

--- src/components/CardActions/themeOverrides.ts
+++ src/components/CardActions/themeOverrides.ts
@@ -3,5 +3,10 @@ import { Components, Theme } from '@mui/material'
 export const MonorailCardActionsOverrides: Components<Theme>['MuiCardActions'] =
   {
     defaultProps: {},
-    styleOverrides: {},
+    styleOverrides: {
+      root: {
+        justifyContent: 'right',
+        padding: '1rem',
+      },
+    },
   }
