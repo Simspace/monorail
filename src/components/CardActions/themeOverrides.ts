cbb314f4a95031a341c97b4e459058dbead7547f
Michael Martin <mlmartin13@gmail.com>
2022-04-26 12:32:55 -0600

    Add all card-related override files

---
+++ src/components/CardActions/themeOverrides.ts
@@ -0,0 +1,7 @@
+import { Components, Theme } from '@mui/material'
+
+export const MonorailCardActionsOverrides: Components<Theme>['MuiCardActions'] =
+  {
+    defaultProps: {},
+    styleOverrides: {},
+  }
---
+++ src/components/CardHeader/themeOverrides.ts
@@ -0,0 +1,6 @@
+import { Components, Theme } from '@mui/material'
+
+export const MonorailCardHeaderOverrides: Components<Theme>['MuiCardHeader'] = {
+  defaultProps: {},
+  styleOverrides: {},
+}
