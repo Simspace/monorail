cdf42dcdbb88f2c9cc4e35f29ce26397c988dd9d
gsamaniego41 <gsamaniego41@gmail.com>
2022-04-27 13:59:25 -0400

    checkbox updates

--- src/components/Checkbox/themeOverrides.ts
+++ src/components/Checkbox/themeOverrides.ts
@@ -5,12 +5,24 @@ import {
   Theme,
 } from '@mui/material'
 
+const checkboxTokens = {
+  shape: 4,
+  bg: {
+    hover: 100,
+  },
+  icon: {
+    idle: 600,
+    hover: 700,
+  },
+} as const
+
 export const MonorailCheckboxOverrides: Components<Theme>['MuiCheckbox'] = {
   defaultProps: {},
   styleOverrides: {
     root: ({ ownerState: { color = 'primary', size = 'medium' }, theme }) => ({
+      color: theme.palette.default.shades[checkboxTokens.icon.idle],
       padding: 8,
-      borderRadius: 3,
+      borderRadius: 4,
       ...(size === 'small' && {
         padding: 6,
         [`& .${svgIconClasses.root}`]: {
@@ -18,12 +30,21 @@ export const MonorailCheckboxOverrides: Components<Theme>['MuiCheckbox'] = {
         },
       }),
       '&:hover': {
-        backgroundColor: theme.palette[color].shades[50],
+        backgroundColor: theme.palette.default.shades[checkboxTokens.bg.hover],
+        [`& .${svgIconClasses.root}`]: {
+          color: theme.palette.default.shades[checkboxTokens.icon.hover],
+        },
       },
       [`&.${checkboxClasses.checked}`]: {
         color: theme.palette[color].main,
         [`&.${checkboxClasses.disabled}`]: {
           color: theme.palette.action.disabled,
+        },
+        '&:hover': {
+          backgroundColor: theme.palette[color].shades[checkboxTokens.bg.hover],
+          [`& .${svgIconClasses.root}`]: {
+            color: theme.palette[color].shades[checkboxTokens.icon.hover],
+          },
         },
       },
       [`&.Mui-focusVisible`]: {
