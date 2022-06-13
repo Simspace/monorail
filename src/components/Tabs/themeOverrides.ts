cd033775552c701e8c06ff2bc332055a19efe1eb
gsamaniego41 <gsamaniego41@gmail.com>
2022-06-13 16:42:29 -0400

    fix horizontal indicator border

--- src/components/Tabs/themeOverrides.ts
+++ src/components/Tabs/themeOverrides.ts
@@ -1,16 +1,16 @@
-import { Components, Theme } from '@mui/material'
-
-import { isNotNil } from '../../test-helpers/typeGuards'
+import { Components, tabsClasses, Theme } from '@mui/material'
 
 export const MonorailTabsOverrides: Components<Theme>['MuiTabs'] = {
   defaultProps: {},
   styleOverrides: {
-    indicator: ({ ownerState: { vertical = false } }) => ({
+    indicator: {
       height: 3,
       borderRadius: `3px 3px 0 0`,
-      ...(isNotNil(vertical) && {
+    },
+    vertical: {
+      [`& .${tabsClasses.indicator}`]: {
         borderRadius: `3px 0 0 3px`,
-      }),
-    }),
+      },
+    },
   },
 }
