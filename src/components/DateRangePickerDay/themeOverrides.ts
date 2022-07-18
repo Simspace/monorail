142f439379c96c9f35f3e5ad6159cadd6bd6a3e6
Gabe Samaniego <gabe@simspace.com>
2022-07-18 17:38:14 +0000

    Date Range Picker (MUI X)

---
+++ src/components/DateRangePickerDay/themeOverrides.ts
@@ -0,0 +1,100 @@
+import { dateRangePickerDayClasses, pickersDayClasses } from '@mui/lab'
+import { buttonBaseClasses, Components, Theme } from '@mui/material'
+
+export const MonorailDateRangePickerDayOverrides: Components<Theme>['MuiDateRangePickerDay'] =
+  {
+    defaultProps: {},
+    styleOverrides: {
+      root: ({ theme }) => {
+        const BORDER_RADIUS = 4
+        const classes = {
+          /**
+           * (Default) Day on the calendar that is not selected
+           */
+          unselectedDay: `& .${buttonBaseClasses.root}.${dateRangePickerDayClasses.day}.${dateRangePickerDayClasses.dayOutsideRangeInterval}.${dateRangePickerDayClasses.notSelectedDate}.${pickersDayClasses.root}`,
+          /**
+           * Individual day in between a selected range
+           */
+          dayInsideRangeUnselected: `& .${buttonBaseClasses.root}.${dateRangePickerDayClasses.day}.${dateRangePickerDayClasses.dayInsideRangeInterval}.${dateRangePickerDayClasses.notSelectedDate}.${pickersDayClasses.root}`,
+          /**
+           * Grey dashed border when selecting a range
+           */
+          rangePreviewBorder: `& .${dateRangePickerDayClasses.rangeIntervalPreview}.${dateRangePickerDayClasses.rangeIntervalDayPreview}`,
+          /**
+           * Start of highlighted range, selected (dark blue) or unselected (light blue)
+           */
+          rangeHighlightStart: `&.${dateRangePickerDayClasses.rangeIntervalDayHighlightStart}`,
+          /**
+           * End of highlighted range, selected (dark blue) or unselected (light blue)
+           */
+          rangeHighlightEnd: `&.${dateRangePickerDayClasses.rangeIntervalDayHighlightEnd}`,
+        }
+
+        return {
+          '& button': {
+            borderRadius: BORDER_RADIUS,
+            '&:hover': {
+              borderColor: 'transparent',
+            },
+          },
+          // Start of each selected row
+          '&:first-of-type': {
+            borderTopLeftRadius: BORDER_RADIUS,
+            borderBottomLeftRadius: BORDER_RADIUS,
+          },
+          // End of each selected row
+          '&:last-of-type': {
+            borderTopRightRadius: BORDER_RADIUS,
+            borderBottomRightRadius: BORDER_RADIUS,
+          },
+          [classes.unselectedDay]: {
+            '&:hover': {
+              border: `1px solid ${theme.palette.primary.border.light}`,
+              backgroundColor: 'transparent',
+            },
+            '&:active': {
+              border: `1px solid ${theme.palette.primary.border.main}`,
+              backgroundColor: theme.palette.primary.mediumEmphasis.light,
+            },
+            // Start day selected
+            [classes.unselectedDay + '.Mui-selected']: {
+              '&:hover': {
+                backgroundColor: theme.palette.primary.hover,
+              },
+              '&:active': {
+                backgroundColor: theme.palette.primary.active,
+              },
+            },
+          },
+          [classes.dayInsideRangeUnselected]: {
+            '&:hover': {
+              border: `1px solid ${theme.palette.primary.border.light}`,
+              backgroundColor: theme.palette.primary.mediumEmphasis.main,
+            },
+            '&:active': {
+              border: `1px solid ${theme.palette.primary.border.main}`,
+              backgroundColor: theme.palette.primary.mediumEmphasis.dark,
+            },
+          },
+          [`&.${dateRangePickerDayClasses.rangeIntervalDayHighlight}`]: {
+            backgroundColor: theme.palette.primary.mediumEmphasis.main,
+            '&:first-of-type': {
+              borderTopLeftRadius: BORDER_RADIUS,
+              borderBottomLeftRadius: BORDER_RADIUS,
+            },
+          },
+          [classes.rangePreviewBorder]: {
+            borderRadius: BORDER_RADIUS,
+          },
+          [classes.rangeHighlightStart]: {
+            borderTopLeftRadius: BORDER_RADIUS,
+            borderBottomLeftRadius: BORDER_RADIUS,
+          },
+          [classes.rangeHighlightEnd]: {
+            borderTopRightRadius: BORDER_RADIUS,
+            borderBottomRightRadius: BORDER_RADIUS,
+          },
+        }
+      },
+    },
+  }
