087ab91a57729495868f179a8d9e15814c2f7473
Peter Krol <pkrol@simspace.com>
2022-08-17 17:57:17 -0400

    fix: change moved props import

--- src/components/EmphaticFormControlLabel/themeAugmentation/props.ts
+++ src/components/EmphaticFormControlLabel/themeAugmentation/props.ts
@@ -1,4 +1,4 @@
-import { EmphaticFormControlLabelProps } from '../EmphaticFormControlLabel'
+import { EmphaticFormControlLabelProps } from '../emphaticFormControlLabelProps'
 
 declare module '@mui/material/styles/props' {
   interface ComponentsPropsList {
