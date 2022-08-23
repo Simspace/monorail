4d8477386178af89564cf96bb20e712a2fddba7c
Peter Krol <pkrol@simspace.com>
2022-08-23 19:33:42 -0400

    fix(jest): resolve relative modules as extensionless

--- packages/storybook/jest/enhancedResolver.cjs
+++ packages/storybook/jest/enhancedResolver.cjs
@@ -1,11 +1,13 @@
 const resolver = require('enhanced-resolve').create.sync({
-  extensions: [".js", ".jsx", ".ts", ".tsx"],
-  mainFields: ["exports", "import", "require", "module", "main"],
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  mainFields: ['exports', 'import', 'require', 'module', 'main'],
   conditionNames: ['require', 'node', 'default', 'import'],
 })
 
 function resolve(request, options) {
-  if(request.indexOf('@monorail') === 0){
+  if (!options.basedir.includes('node_modules') && request.startsWith('.')) {
+    return options.defaultResolver(request.replace('.js', ''), options)
+  } else if (request.indexOf('@monorail') === 0) {
     return resolver(options.basedir, request)
   } else {
     return options.defaultResolver(request, options)
