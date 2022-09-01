b1482cdb372fe3d30c0b6e29edea3dd16b901ef4
Casey Hunt <casey.hunt@simspace.com>
2022-09-01 16:05:24 -0500

    Adds a responsive screen option to Playroom config

--- playroom.config.js
+++ playroom.config.js
@@ -3,6 +3,7 @@ module.exports = {
   themes: "./packages/themes/src/index.ts",
   outputPath: "./playroom",
   frameComponent: "./.playroom/Frame.jsx",
+  widths: [320, 375, 768, 1024, 'calc(100vw - 120px)'],
   title: "Monorail Playroom",
   exampleCode: `
     <Button>
