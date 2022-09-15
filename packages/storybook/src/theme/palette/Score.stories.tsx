3f3eed18c0d6c74f97c8cccef8716cfdb142225a
gsamaniego41 <gsamaniego41@gmail.com>
2022-09-15 10:15:01 -0400

    fix: handle color palette doc errors

--- packages/storybook/src/theme/palette/Score.stories.tsx
+++ packages/storybook/src/theme/palette/Score.stories.tsx
@@ -22,150 +22,159 @@ export const Score = () => {
     [theme.name],
   )
 
-  const scoreColors: Array<{
-    name: string
-    data: Array<ColorTokenRowProps>
-  }> = [
-    {
-      name: 'Low',
-      data: [
-        {
-          token: '.low.light',
-          colorValue: theme.palette.score.low.light,
-          figmaStyle: 'Score/Low/Light',
-        },
-        {
-          token: '.low.main',
-          colorValue: theme.palette.score.low.main,
-          figmaStyle: 'Score/Low/Main',
-        },
-        {
-          token: '.low.dark',
-          colorValue: theme.palette.score.low.dark,
-          figmaStyle: 'Score/Low/Dark',
-        },
-        {
-          token: '.low.contrastText',
-          colorValue: theme.palette.score.low.contrastText,
-          figmaStyle: 'Score/Low/Contrast Text',
-          description:
-            'Use for text or icons on containers with score background colors.',
-        },
-      ],
-    },
-    {
-      name: 'Low Moderate',
-      data: [
-        {
-          token: '.lowModerate.light',
-          colorValue: theme.palette.score.lowModerate.light,
-          figmaStyle: 'Score/Low Moderate/Light',
-        },
-        {
-          token: '.lowModerate.main',
-          colorValue: theme.palette.score.lowModerate.main,
-          figmaStyle: 'Score/Low Moderate/Main',
-        },
-        {
-          token: '.lowModerate.dark',
-          colorValue: theme.palette.score.lowModerate.dark,
-          figmaStyle: 'Score/Low Moderate/Dark',
-        },
-        {
-          token: '.lowModerate.contrastText',
-          colorValue: theme.palette.score.lowModerate.contrastText,
-          figmaStyle: 'Score/Low Moderate/Contrast Text',
-          description:
-            'Use for text or icons on containers with score background colors.',
-        },
-      ],
-    },
-    {
-      name: 'Moderate',
-      data: [
-        {
-          token: '.moderate.light',
-          colorValue: theme.palette.score.moderate.light,
-          figmaStyle: 'Score/Moderate/Light',
-        },
-        {
-          token: '.moderate.main',
-          colorValue: theme.palette.score.moderate.main,
-          figmaStyle: 'Score/Moderate/Main',
-        },
-        {
-          token: '.moderate.dark',
-          colorValue: theme.palette.score.moderate.dark,
-          figmaStyle: 'Score/Moderate/Dark',
-        },
-        {
-          token: '.moderate.contrastText',
-          colorValue: theme.palette.score.moderate.contrastText,
-          figmaStyle: 'Score/Moderate/Contrast Text',
-          description:
-            'Use for text or icons on containers with score background colors.',
-        },
-      ],
-    },
-    {
-      name: 'High Moderate',
-      data: [
-        {
-          token: '.highModerate.light',
-          colorValue: theme.palette.score.highModerate.light,
-          figmaStyle: 'Score/High Moderate/Light',
-        },
-        {
-          token: '.highModerate.main',
-          colorValue: theme.palette.score.highModerate.main,
-          figmaStyle: 'Score/High Moderate/Main',
-        },
-        {
-          token: '.highModerate.dark',
-          colorValue: theme.palette.score.highModerate.dark,
-          figmaStyle: 'Score/High Moderate/Dark',
-        },
-        {
-          token: '.highModerate.contrastText',
-          colorValue: theme.palette.score.highModerate.contrastText,
-          figmaStyle: 'Score/High Moderate/Contrast Text',
-          description:
-            'Use for text or icons on containers with score background colors.',
-        },
-      ],
-    },
-    {
-      name: 'High',
-      data: [
-        {
-          token: '.high.light',
-          colorValue: theme.palette.score.high.light,
-          figmaStyle: 'Score/High/Light',
-        },
-        {
-          token: '.high.main',
-          colorValue: theme.palette.score.high.main,
-          figmaStyle: 'Score/High/Main',
-        },
-        {
-          token: '.high.dark',
-          colorValue: theme.palette.score.high.dark,
-          figmaStyle: 'Score/High/Dark',
-        },
-        {
-          token: '.high.contrastText',
-          colorValue: theme.palette.score.high.contrastText,
-          figmaStyle: 'Score/High/Contrast Text',
-          description:
-            'Use for text or icons on containers with score background colors.',
-        },
-      ],
-    },
-  ]
+  const scoreColors:
+    | Array<{
+        name: string
+        data: Array<ColorTokenRowProps>
+      }>
+    | undefined = React.useMemo(() => {
+    if (theme.palette.score === undefined) {
+      return []
+    }
+    return [
+      {
+        name: 'Low',
+        data: [
+          {
+            token: '.low.light',
+            colorValue: theme.palette.score.low.light,
+            figmaStyle: 'Score/Low/Light',
+          },
+          {
+            token: '.low.main',
+            colorValue: theme.palette.score.low.main,
+            figmaStyle: 'Score/Low/Main',
+          },
+          {
+            token: '.low.dark',
+            colorValue: theme.palette.score.low.dark,
+            figmaStyle: 'Score/Low/Dark',
+          },
+          {
+            token: '.low.contrastText',
+            colorValue: theme.palette.score.low.contrastText,
+            figmaStyle: 'Score/Low/Contrast Text',
+            description:
+              'Use for text or icons on containers with score background colors.',
+          },
+        ],
+      },
+      {
+        name: 'Low Moderate',
+        data: [
+          {
+            token: '.lowModerate.light',
+            colorValue: theme.palette.score.lowModerate.light,
+            figmaStyle: 'Score/Low Moderate/Light',
+          },
+          {
+            token: '.lowModerate.main',
+            colorValue: theme.palette.score.lowModerate.main,
+            figmaStyle: 'Score/Low Moderate/Main',
+          },
+          {
+            token: '.lowModerate.dark',
+            colorValue: theme.palette.score.lowModerate.dark,
+            figmaStyle: 'Score/Low Moderate/Dark',
+          },
+          {
+            token: '.lowModerate.contrastText',
+            colorValue: theme.palette.score.lowModerate.contrastText,
+            figmaStyle: 'Score/Low Moderate/Contrast Text',
+            description:
+              'Use for text or icons on containers with score background colors.',
+          },
+        ],
+      },
+      {
+        name: 'Moderate',
+        data: [
+          {
+            token: '.moderate.light',
+            colorValue: theme.palette.score.moderate.light,
+            figmaStyle: 'Score/Moderate/Light',
+          },
+          {
+            token: '.moderate.main',
+            colorValue: theme.palette.score.moderate.main,
+            figmaStyle: 'Score/Moderate/Main',
+          },
+          {
+            token: '.moderate.dark',
+            colorValue: theme.palette.score.moderate.dark,
+            figmaStyle: 'Score/Moderate/Dark',
+          },
+          {
+            token: '.moderate.contrastText',
+            colorValue: theme.palette.score.moderate.contrastText,
+            figmaStyle: 'Score/Moderate/Contrast Text',
+            description:
+              'Use for text or icons on containers with score background colors.',
+          },
+        ],
+      },
+      {
+        name: 'High Moderate',
+        data: [
+          {
+            token: '.highModerate.light',
+            colorValue: theme.palette.score.highModerate.light,
+            figmaStyle: 'Score/High Moderate/Light',
+          },
+          {
+            token: '.highModerate.main',
+            colorValue: theme.palette.score.highModerate.main,
+            figmaStyle: 'Score/High Moderate/Main',
+          },
+          {
+            token: '.highModerate.dark',
+            colorValue: theme.palette.score.highModerate.dark,
+            figmaStyle: 'Score/High Moderate/Dark',
+          },
+          {
+            token: '.highModerate.contrastText',
+            colorValue: theme.palette.score.highModerate.contrastText,
+            figmaStyle: 'Score/High Moderate/Contrast Text',
+            description:
+              'Use for text or icons on containers with score background colors.',
+          },
+        ],
+      },
+      {
+        name: 'High',
+        data: [
+          {
+            token: '.high.light',
+            colorValue: theme.palette.score.high.light,
+            figmaStyle: 'Score/High/Light',
+          },
+          {
+            token: '.high.main',
+            colorValue: theme.palette.score.high.main,
+            figmaStyle: 'Score/High/Main',
+          },
+          {
+            token: '.high.dark',
+            colorValue: theme.palette.score.high.dark,
+            figmaStyle: 'Score/High/Dark',
+          },
+          {
+            token: '.high.contrastText',
+            colorValue: theme.palette.score.high.contrastText,
+            figmaStyle: 'Score/High/Contrast Text',
+            description:
+              'Use for text or icons on containers with score background colors.',
+          },
+        ],
+      },
+    ]
+  }, [theme.palette])
 
-  return (
-    <Box gap={4} sx={{ p: 4 }}>
-      {scoreColors.map(score => (
+  const renderScoreColors =
+    scoreColors.length <= 0 ? (
+      <Typography>{`${theme.name} does not have Score colors.`}</Typography>
+    ) : (
+      scoreColors.map(score => (
         <React.Fragment key={`score-${score.name}`}>
           <Typography variant="h2" gutterBottom>
             {score.name}
@@ -176,7 +185,12 @@ export const Score = () => {
             rawColorObj={rawColorMapping}
           />
         </React.Fragment>
-      ))}
+      ))
+    )
+
+  return (
+    <Box gap={4} sx={{ p: 4 }}>
+      {renderScoreColors}
     </Box>
   )
 }
--- packages/storybook/src/theme/palette/Tiers.stories.tsx
+++ packages/storybook/src/theme/palette/Tiers.stories.tsx
@@ -23,36 +23,46 @@ export const Tiers = () => {
     [theme.name],
   )
 
-  const tiers: Array<ColorTokenRowProps> = [
-    {
-      token: '.one',
-      colorValue: theme.palette.tiers.one,
-      figmaStyle: 'Tiers/One',
-    },
-    {
-      token: '.two',
-      colorValue: theme.palette.tiers.two,
-      figmaStyle: 'Tiers/Two',
-    },
-    {
-      token: '.three',
-      colorValue: theme.palette.tiers.three,
-      figmaStyle: 'Tiers/Three',
-    },
-    {
-      token: '.four',
-      colorValue: theme.palette.tiers.four,
-      figmaStyle: 'Tiers/Four',
-    },
-  ]
+  const tiers: Array<ColorTokenRowProps> = React.useMemo(() => {
+    if (theme.palette.tiers === undefined) {
+      return []
+    }
+    return [
+      {
+        token: '.one',
+        colorValue: theme.palette.tiers.one,
+        figmaStyle: 'Tiers/One',
+      },
+      {
+        token: '.two',
+        colorValue: theme.palette.tiers.two,
+        figmaStyle: 'Tiers/Two',
+      },
+      {
+        token: '.three',
+        colorValue: theme.palette.tiers.three,
+        figmaStyle: 'Tiers/Three',
+      },
+      {
+        token: '.four',
+        colorValue: theme.palette.tiers.four,
+        figmaStyle: 'Tiers/Four',
+      },
+    ]
+  }, [theme.palette])
 
-  return (
-    <Box p={4}>
-      <Typography variant="h2" gutterBottom>
-        Tiers
-      </Typography>
-      <Typography gutterBottom>{`theme.palette.tiers`}</Typography>
-      <ColorTokenTable colorMetadata={tiers} rawColorObj={rawColorMapping} />
-    </Box>
-  )
+  const renderTierColors =
+    tiers.length <= 0 ? (
+      <Typography>{`${theme.name} does not have Tier colors.`}</Typography>
+    ) : (
+      <>
+        <Typography variant="h2" gutterBottom>
+          Tiers
+        </Typography>
+        <Typography gutterBottom>{`theme.palette.tiers`}</Typography>
+        <ColorTokenTable colorMetadata={tiers} rawColorObj={rawColorMapping} />
+      </>
+    )
+
+  return <Box p={4}>{renderTierColors}</Box>
 }
