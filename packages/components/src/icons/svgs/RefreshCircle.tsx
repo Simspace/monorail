06a4598cf4f68738763b359c58e4a6528bbe30ba
gsamaniego41 <gsamaniego41@gmail.com>
2022-09-27 16:01:39 -0400

    add gutter around error icons

--- packages/components/src/icons/svgs/RefreshCircle.tsx
+++ packages/components/src/icons/svgs/RefreshCircle.tsx
@@ -3,12 +3,13 @@ import type { SvgIconProps } from '@mui/material/SvgIcon'
 import SvgIcon from '@mui/material/SvgIcon' // eslint-disable-line no-restricted-imports
 
 export const RefreshCircle = (props: SvgIconProps) => (
-  <SvgIcon viewBox="0 0 120 120" {...props}>
-    <circle cx="60" cy="60" r="60" fill="white" />
+  <SvgIcon {...props}>
+    <circle cx="12" cy="12" r="10" />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M0 60C0 26.88 26.88 0 60 0C93.12 0 120 26.88 120 60C120 93.12 93.12 120 60 120C26.88 120 0 93.12 0 60ZM60 28C68.84 28 76.8 31.6 82.6 37.4L92 28V56H64L76.88 43.12C72.56 38.76 66.64 36 60 36C46.76 36 36 46.76 36 60C36 73.24 46.76 84 60 84C70.44 84 79.32 77.32 82.6 68H90.92C87.36 81.8 74.92 92 60 92C42.32 92 28.04 77.68 28.04 60C28.04 42.32 42.32 28 60 28Z"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12 6.66667C13.4733 6.66667 14.8 7.26667 15.7667 8.23333L17.3333 6.66667V11.3333H12.6667L14.8133 9.18667C14.0933 8.46 13.1067 8 12 8C9.79333 8 8 9.79333 8 12C8 14.2067 9.79333 16 12 16C13.74 16 15.22 14.8867 15.7667 13.3333H17.1533C16.56 15.6333 14.4867 17.3333 12 17.3333C9.05333 17.3333 6.67333 14.9467 6.67333 12C6.67333 9.05333 9.05333 6.66667 12 6.66667Z"
+      fill="white"
     />
   </SvgIcon>
 )
