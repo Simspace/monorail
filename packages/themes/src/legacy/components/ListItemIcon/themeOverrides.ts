b5287727de656de9b0973efb93599572c8609664
Peter Krol <pkrol@simspace.com>
2023-02-06 11:41:09 -0500

    chore: remove commented line

--- packages/themes/src/legacy/components/ListItemIcon/themeOverrides.ts
+++ packages/themes/src/legacy/components/ListItemIcon/themeOverrides.ts
@@ -8,7 +8,6 @@ export const MonorailListItemIconOverrides: Components<Theme>['MuiListItemIcon']
       root: ({ theme }) => ({
         minWidth: 36,
         paddingLeft: theme.spacing(1),
-        // marginLeft: theme.spacing(2),
         color: theme.palette.default.main,
         [`& > .${checkboxClasses.root}`]: {
           marginLeft: theme.spacing(-1),
