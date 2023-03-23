d366fb9ffd686f66804b947217cd74f56390f4b4
gsamaniego41 <gsamaniego41@gmail.com>
2023-03-23 13:19:50 -0400

    refactor: update file name to meteor

--- packages/themes/src/index.ts
+++ packages/themes/src/index.ts
@@ -1,7 +1,7 @@
 // codegen:start { preset: barrel, include: ./*.ts }
 export * from './classic.js'
-export * from './halloween.js'
 export * from './legacy.js'
+export * from './meteor.js'
 export * from './mui.js'
 export * from './pcte.js'
 // codegen:end
