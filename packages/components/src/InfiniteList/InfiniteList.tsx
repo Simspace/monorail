3e63795a7386b7f0557ac3bcfc480956747ab1f3
Peter Krol <pkrol@simspace.com>
2023-04-06 23:40:49 -0400

    refactor: implement InfiniteList with react-virtuoso

--- packages/components/src/InfiniteList/InfiniteList.tsx
+++ packages/components/src/InfiniteList/InfiniteList.tsx
@@ -1,193 +1,110 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable eqeqeq */
 import React from 'react'
-import type {
-  FixedSizeListProps,
-  ListOnItemsRenderedProps,
-  VariableSizeList,
-  VariableSizeListProps,
-} from 'react-window'
+import { Virtuoso } from 'react-virtuoso'
 
+import { Box } from '../Box.js'
 import { CircularProgress } from '../CircularProgress.js'
-import type { ListProps } from '../List.js'
 import { List } from '../List.js'
 import { ListItem } from '../ListItem.js'
-import type {
-  InfiniteListItemProps,
-  InfiniteListProps,
-} from './InfiniteListProps.js'
-
-interface InfiniteListState {
-  lastLoadedIndex: number
-  isLoadingNextPage: boolean
-}
-
-interface ListRef {
-  resetAfterIndex?: VariableSizeList['resetAfterIndex']
-  _getItemStyleCache?: (_: number) => void
-  forceUpdate: () => void
-  reset: (autoReload?: boolean) => void
-}
-
-const Loader = (props: InfiniteListItemProps<unknown>) => {
-  const { style } = props
-  return (
-    <ListItem
-      style={style}
-      sx={{ alignItems: 'center', justifyContent: 'center' }}
-    >
-      <CircularProgress />
-    </ListItem>
-  )
-}
+import type { InfiniteListProps } from './InfiniteListProps.js'
 
 export const InfiniteList = React.forwardRef(function InfiniteList(
-  props: InfiniteListProps<unknown, React.ComponentType<FixedSizeListProps>>,
+  props: InfiniteListProps<
+    unknown,
+    {
+      slots: InfiniteListProps['slots']
+      slotProps: InfiniteListProps['slotProps']
+    }
+  >,
   ref,
 ) {
-  const {
-    component,
-    isItemLoaded,
-    loadNextPage,
-    renderItem,
-    itemSize,
-    width,
-    height,
-    data,
-    pageSize = 10,
-    threshold = 5,
-    slots = {},
-    slotProps = {},
-    ...others
-  } = props
-
-  const VirtualizedListComponent = component
-  const LoaderComponent = slots.loader ?? Loader
-  const ListComponent = React.useMemo(() => {
-    if (slots.list) {
-      return (props: Record<string, any>) =>
-        React.createElement(slots.list as React.ComponentType<ListProps>, {
-          ...slotProps.list,
-          ...props,
-        })
-    }
-    return (props: Record<string, any>) => (
-      <List {...slotProps.list} {...props} />
-    )
-  }, [slotProps.list, slots.list])
-
-  const state = React.useRef<InfiniteListState>({
-    lastLoadedIndex: -1,
-    isLoadingNextPage: false,
-  })
-
-  React.useImperativeHandle(ref, () => listRef.current)
-
-  const listRef: React.MutableRefObject<ListRef | null> = React.useRef(null)
-
-  const loadPage = React.useCallback(() => {
-    if (!state.current.isLoadingNextPage) {
-      state.current.isLoadingNextPage = true
-      const promise = loadNextPage()
-      if (promise != null) {
-        promise.then(() => {
-          if (listRef.current != null) {
-            if (typeof listRef.current.resetAfterIndex === 'function') {
-              listRef.current.resetAfterIndex(
-                state.current.lastLoadedIndex,
-                true,
-              )
-            } else {
-              if (typeof listRef.current._getItemStyleCache === 'function') {
-                listRef.current._getItemStyleCache(-1)
-              }
-              listRef.current.forceUpdate()
-            }
-          }
-          state.current.lastLoadedIndex += pageSize
-          state.current.isLoadingNextPage = false
-        })
-      }
-    }
-  }, [loadNextPage, pageSize])
-
-  const itemCount = React.useMemo(() => {
-    if (!isItemLoaded(data.length)) {
-      return data.length + 1
-    }
-    return data.length - 1
-  }, [data, isItemLoaded])
-
-  const ensureRowsLoaded = React.useCallback(
-    ({
-      visibleStopIndex,
-    }: Pick<ListOnItemsRenderedProps, 'visibleStopIndex'>) => {
-      let endIndex = visibleStopIndex
-
-      if (endIndex + threshold > state.current.lastLoadedIndex) {
-        endIndex = state.current.lastLoadedIndex
-      }
-
-      if (!isItemLoaded(endIndex + 1)) {
-        loadPage()
-      }
-    },
-    [isItemLoaded, threshold, loadPage],
-  )
-
-  const reset = React.useCallback(
-    (autoReload: boolean = false) => {
-      state.current.lastLoadedIndex = -1
-      state.current.isLoadingNextPage = false
-      if (autoReload) {
-        ensureRowsLoaded({ visibleStopIndex: 0 })
-      }
-    },
-    [ensureRowsLoaded],
-  )
-
-  React.useEffect(() => {
-    if (listRef.current) {
-      listRef.current.reset = reset
-    }
-  }, [reset])
-
-  const Item = (props: InfiniteListItemProps) => {
-    if (!isItemLoaded(props.index)) {
-      return <LoaderComponent {...props} />
-    }
-    return renderItem({ ...props, data: data[props.index] })
-  }
-
-  const setListRef = React.useCallback(
-    (list: ListRef | null) => {
-      listRef.current = list
-      if (list !== null) {
-        listRef.current!.reset = reset
-      }
-    },
-    [reset],
-  )
+  const { data, slots = {}, slotProps = {}, ...others } = props
 
   return (
-    <VirtualizedListComponent
-      ref={setListRef}
-      itemSize={itemSize}
-      width={width}
-      height={height}
-      onItemsRendered={ensureRowsLoaded}
-      itemCount={itemCount}
-      innerElementType={ListComponent}
+    <Virtuoso
+      ref={ref}
+      data={data}
       {...others}
-    >
-      {Item}
-    </VirtualizedListComponent>
+      context={{
+        slots,
+        slotProps,
+        ...others.context,
+      }}
+      components={{
+        List: ListComponent,
+        Footer: LoaderComponent,
+        Item: ListItemComponent,
+        ...others.components,
+      }}
+    />
   )
-}) as unknown as <
-  T,
-  C extends
-    | React.ComponentType<FixedSizeListProps<T>>
-    | React.ComponentType<VariableSizeListProps<T>>,
->(
-  props: InfiniteListProps<T, C>,
-) => JSX.Element
+}) as <T, C>(props: InfiniteListProps<T, C>) => JSX.Element
+
+const DefaultLoaderComponent = React.forwardRef(function DefaultLoaderComponent(
+  props,
+  ref,
+) {
+  return (
+    <Box
+      ref={ref}
+      py={4}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      {...props}
+    >
+      <CircularProgress />
+    </Box>
+  )
+})
+
+interface SlotComponentProps {
+  ref?: React.Ref<any>
+  context?: {
+    slots?: InfiniteListProps['slots']
+    slotProps?: InfiniteListProps['slotProps']
+  }
+}
+
+const LoaderComponent = React.forwardRef(function LoaderComponent(props, ref) {
+  const { context = {}, ...others } = props
+
+  let LoaderComponent: React.ComponentType<{ ref?: React.Ref<any> }> =
+    DefaultLoaderComponent
+
+  if (context.slots?.loader) {
+    LoaderComponent = context.slots.loader
+  }
+
+  return (
+    <LoaderComponent {...others} {...context.slotProps?.loader} ref={ref} />
+  )
+}) as (props: SlotComponentProps) => JSX.Element
+
+const ListComponent = React.forwardRef(function ListComponent(props, ref) {
+  const { context = {}, ...others } = props
+
+  let ListComponent: React.ComponentType<{ ref?: React.Ref<any> }> = List
+
+  if (context.slots?.list) {
+    ListComponent = context.slots.list
+  }
+
+  return <ListComponent {...others} {...context.slotProps?.list} ref={ref} />
+}) as (props: SlotComponentProps) => JSX.Element
+
+const ListItemComponent = React.forwardRef(function ListComponent(props, ref) {
+  const { context = {}, ...others } = props
+
+  let ListItemComponent: React.ComponentType<{ ref?: React.Ref<any> }> =
+    ListItem
+
+  if (context.slots?.listItem) {
+    ListItemComponent = context.slots.listItem
+  }
+
+  return (
+    <ListItemComponent {...others} {...context.slotProps?.listItem} ref={ref} />
+  )
+}) as (props: SlotComponentProps) => JSX.Element
--- packages/components/src/InfiniteList/InfiniteListProps.ts
+++ packages/components/src/InfiniteList/InfiniteListProps.ts
@@ -1,66 +1,16 @@
-import type {
-  FixedSizeListProps,
-  ListChildComponentProps,
-  VariableSizeListProps,
-} from 'react-window'
+import type { VirtuosoHandle, VirtuosoProps } from 'react-virtuoso'
 
-import type { ListProps } from '../List.js'
-import type { ListItemProps } from '../ListItem.js'
-
-export interface InfiniteListItemProps<T = unknown>
-  extends ListChildComponentProps<T>,
-    Omit<ListItemProps, 'style'> {}
-
-export type InfiniteListProps<
-  T = unknown,
-  C extends
-    | React.ComponentType<FixedSizeListProps<T>>
-    | React.ComponentType<VariableSizeListProps<T>> = React.ComponentType<
-    FixedSizeListProps<T>
-  >,
-> = Omit<
-  C extends React.ComponentType<infer P> ? P : {},
-  'itemData' | 'children' | 'itemCount'
-> & {
-  ref?: React.RefObject<C & { reset: (autoReload?: boolean) => void }>
-  /**
-   * The virtualized list component to use
-   * @default FixedSizeList
-   */
-  component: C
-  /**
-   * Row data
-   */
-  data: ReadonlyArray<T>
-  /**
-   * Determines whether the item at the given index is loaded
-   */
-  isItemLoaded: (index: number) => boolean
-  /**
-   * A function used to render a single row
-   */
-  renderItem: (props: InfiniteListItemProps<T>) => React.ReactElement
-  /**
-   * Callback to be invoked when the next page must be loaded.
-   * It should return a Promise that is resolved once all data has finished loading.
-   */
-  loadNextPage: () => Promise<void> | void
-  /**
-   * The number of rows per page
-   */
-  pageSize: number
-  /**
-   * Threshold at which to pre-fetch data.
-   * A threshold of 5 means that data will start loading when a user scrolls within 5 rows.
-   * @default 5
-   */
-  threshold?: number
+export interface InfiniteListProps<T = unknown, C = unknown>
+  extends VirtuosoProps<T, C> {
+  ref?: React.Ref<VirtuosoHandle>
   slots?: {
-    loader?: React.ComponentType<InfiniteListItemProps>
-    list?: React.ComponentType<ListProps>
+    loader?: React.ComponentType<Record<string, unknown>>
+    list?: React.ComponentType<Record<string, unknown>>
+    listItem?: React.ComponentType<Record<string, unknown>>
   }
   slotProps?: {
-    loader?: Partial<InfiniteListItemProps>
-    list?: Partial<ListProps>
+    loader?: Record<string, unknown>
+    list?: Record<string, unknown>
+    listItem?: Record<string, unknown>
   }
 }
