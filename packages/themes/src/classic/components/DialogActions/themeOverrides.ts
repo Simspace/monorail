fdc6a2f1fff84e42457da020e615127dfe02ef92
gsamaniego41 <gsamaniego41@gmail.com>
2023-04-20 19:44:06 -0400

    fix: DialogActions console warning

--- packages/themes/src/classic/components/DialogActions/themeOverrides.ts
+++ packages/themes/src/classic/components/DialogActions/themeOverrides.ts
@@ -2,14 +2,15 @@ import type { Components, Theme } from '@mui/material'
 
 export const MonorailDialogActionsOverrides: Components<Theme>['MuiDialogActions'] =
   {
-    defaultProps: {
-      divider: false,
-    },
+    defaultProps: {},
     styleOverrides: {
       root: ({ theme }) => {
         return {
           padding: theme.spacing(3, 6),
           height: theme.spacing(16),
+          '&.MonorailDialogActions-divider': {
+            borderTop: `1px solid ${theme.palette.divider}`,
+          },
         }
       },
     },
