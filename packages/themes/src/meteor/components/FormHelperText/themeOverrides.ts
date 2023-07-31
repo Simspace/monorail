aa80ae9cfca281e899fe6f4bd2b50fbcee103652
gsamaniego41 <gsamaniego41@gmail.com>
2023-07-31 16:05:41 -0400

    feat: input label and error text color

--- packages/themes/src/meteor/components/FormHelperText/themeOverrides.ts
+++ packages/themes/src/meteor/components/FormHelperText/themeOverrides.ts
@@ -1,15 +1,19 @@
 import type { Components, Theme } from '@mui/material'
+import { formHelperTextClasses } from '@mui/material'
 
 export const MonorailFormHelperTextOverrides: Components<Theme>['MuiFormHelperText'] =
   {
     defaultProps: {},
     styleOverrides: {
-      root: ({ theme }) => ({
+      root: ({  theme }) => ({
         color: theme.palette.default.main,
         marginTop: theme.spacing(0.5),
         marginLeft: theme.spacing(3),
         marginRight: theme.spacing(1),
         ...theme.typography.helperText,
+        [`&.${formHelperTextClasses.error}`]: {
+          color: theme.palette.error.lowEmphasis.contrastText,
+        }
       }),
     },
   }
