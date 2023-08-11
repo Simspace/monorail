1051d91857637b366f8e8df72a60662e89c9e530
gsamaniego41 <gsamaniego41@gmail.com>
2023-08-11 13:07:11 -0400

    feat: meteor style overrides

---
+++ packages/themes/src/meteor/components/FileUpload/themeOverrides.tsx
@@ -0,0 +1,30 @@
+import type { Components, Theme } from '@mui/material'
+
+import { fileUploadClasses } from '@monorail/components'
+
+export const MonorailFileUploadOverrides: Components<Theme>['MonorailFileUpload'] =
+  {
+    defaultProps: {
+      slotProps: {
+        action: {
+          variant: 'outlined',
+        },
+      },
+    },
+    styleOverrides: {
+      icon: ({ ownerState: { status = 'default' }, theme }) => ({
+        ...(status !== 'error' && { color: theme.palette.default.light }),
+      }),
+      dropTarget: ({ theme }) => ({
+        border: `1px solid ${theme.palette.outlinedBorder}`,
+        [`.${fileUploadClasses.statusDefault} &`]: {
+          border: `1px solid ${theme.palette.outlinedBorder}`,
+          backgroundColor: theme.palette.background.paper,
+        },
+        [`.${fileUploadClasses.statusError} &`]: {
+          borderColor: theme.palette.error.main,
+          backgroundColor: theme.palette.background.paper,
+        },
+      }),
+    },
+  }
