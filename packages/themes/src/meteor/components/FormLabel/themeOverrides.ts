179d48bb797246d04c51b5ba26a1ea6c17dc30b6
gsamaniego41 <gsamaniego41@gmail.com>
2023-08-16 15:35:27 -0400

    fix: form label error text color

--- packages/themes/src/meteor/components/FormLabel/themeOverrides.ts
+++ packages/themes/src/meteor/components/FormLabel/themeOverrides.ts
@@ -1,4 +1,5 @@
 import type { Components, Theme } from '@mui/material'
+import { formLabelClasses } from '@mui/material'
 
 export const MonorailFormLabelOverrides: Components<Theme>['MuiFormLabel'] = {
   defaultProps: {},
@@ -7,6 +8,9 @@ export const MonorailFormLabelOverrides: Components<Theme>['MuiFormLabel'] = {
       ...theme.typography.inputLabel,
       position: 'relative',
       left: theme.spacing(3),
+      [`&.${formLabelClasses.error}`]: {
+        color: theme.palette.error.lowEmphasis.contrastText,
+      },
     }),
   },
 }
