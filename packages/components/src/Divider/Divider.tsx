66f7bfabb25366c6775b7df20650fe79ad26aab5
gsamaniego41 <gsamaniego41@gmail.com>
2023-10-20 14:54:54 -0400

    feat: add spacing prop

--- packages/components/src/Divider/Divider.tsx
+++ packages/components/src/Divider/Divider.tsx
@@ -1,10 +1,48 @@
+import React from 'react'
+import type {
+  DividerProps as MuiDividerProps,
+  DividerTypeMap,
+} from '@mui/material'
 import { Divider as MuiDivider } from '@mui/material'
+import clsx from 'clsx'
+
+import type { OverridableComponent } from '@monorail/types'
+import { capitalize } from '@monorail/utils'
+
+export interface DividerExtraProps {
+  /**
+   * Adds spacing to the divider.
+   *
+   * @default false
+   */
+  spacing?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false
+}
+
+export type DividerProps<
+  D extends React.ElementType = DividerTypeMap['defaultComponent'],
+  P = {},
+> = MuiDividerProps<D, DividerExtraProps & P>
 
 /**
  * Demos:
  *
  * - [Divider](https://simspace.gitlab.io/engineering/ux-engineering/monorail/main/storybook/?path=/docs/data-display-divider--default)
  */
-export const Divider: typeof MuiDivider = MuiDivider
+export const Divider = React.forwardRef(
+  ({ className, spacing = false, ...props }, ref) => (
+    <MuiDivider
+      ref={ref}
+      className={clsx(
+        spacing !== false && `MonorailDivider-spacing${capitalize(spacing)}`,
+        className,
+      )}
+      {...props}
+    />
+  ),
+) as OverridableComponent<DividerTypeMap<DividerExtraProps, 'hr'>>
+
+// @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+Divider.muiName = MuiDivider.muiName
 
 export * from '@mui/material/Divider'
