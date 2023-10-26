6175e37f812b6fd6b9df6e35a04501e815b28e8f
gsamaniego41 <gsamaniego41@gmail.com>
2023-10-26 15:02:34 -0400

    feat: stories

--- packages/storybook/src/ListSubheader/ListSubheader.stories.tsx
+++ packages/storybook/src/ListSubheader/ListSubheader.stories.tsx
@@ -2,7 +2,13 @@
 import React from 'react'
 
 import type { ListSubheaderProps } from '@monorail/components'
-import { ListSubheader } from '@monorail/components'
+import {
+  IconButton,
+  ListSubheader,
+  Stack,
+  Typography,
+} from '@monorail/components'
+import { Add, Close } from '@monorail/components/icons'
 
 import { story } from '../helpers/storybook.js'
 
@@ -21,3 +27,45 @@ const Template = story<ListSubheaderProps>(
 )
 
 export const Default = story(Template)
+
+/**
+ * Additional elements are pushed to the right by default.
+ */
+export const WithActionButtons = story<ListSubheaderProps>(args => (
+  <ul>
+    <ListSubheader {...args} sx={{ maxWidth: 300 }}>
+      <Typography variant="inherit">List Subheader</Typography>
+      <IconButton onClick={() => {}} aria-hidden>
+        <Add />
+      </IconButton>
+    </ListSubheader>
+    <ListSubheader {...args} sx={{ maxWidth: 300 }}>
+      <Typography variant="inherit">List Subheader</Typography>
+      <Stack direction="row">
+        <IconButton onClick={() => {}} aria-hidden>
+          <Close />
+        </IconButton>
+        <IconButton onClick={() => {}} aria-hidden>
+          <Add />
+        </IconButton>
+      </Stack>
+    </ListSubheader>
+  </ul>
+))
+
+/**
+ * Wrap the subheader text in a Typography component to enable line-clamping and to keep other elements from being hidden.
+ */
+export const TextOverflow = story<ListSubheaderProps>(args => (
+  <ul>
+    <ListSubheader {...args} sx={{ maxWidth: 300 }}>
+      <Typography variant="inherit" lineClamp={2}>
+        A long subheader text will wrap and clamp when it reaches two lines of
+        text
+      </Typography>
+      <IconButton onClick={() => {}} aria-hidden>
+        <Add />
+      </IconButton>
+    </ListSubheader>
+  </ul>
+))
