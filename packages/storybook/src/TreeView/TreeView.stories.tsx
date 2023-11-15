67f5a2c590e20f9c8ace0d212f2502e4894b0856
gsamaniego41 <gsamaniego41@gmail.com>
2023-11-15 13:16:29 -0500

    feat: add stories

--- packages/storybook/src/TreeView/TreeView.stories.tsx
+++ packages/storybook/src/TreeView/TreeView.stories.tsx
@@ -484,6 +484,7 @@ export const CustomizedTreeView = story(() => {
   const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
     color: theme.palette.text.secondary,
     [`& .${treeItemClasses.content}`]: {
+      alignItems: 'center',
       color: theme.palette.text.secondary,
       borderTopRightRadius: theme.spacing(2),
       borderBottomRightRadius: theme.spacing(2),
@@ -525,7 +526,15 @@ export const CustomizedTreeView = story(() => {
     return (
       <StyledTreeItemRoot
         label={
-          <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
+          <Box
+            sx={{
+              display: 'flex',
+              flex: 1,
+              alignItems: 'center',
+              p: 0.5,
+              pr: 0,
+            }}
+          >
             <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
             <Typography
               variant="body2"
