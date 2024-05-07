ce0c16ec7cc29595ef4716ab8f3c036f32e2b08e
Peter Krol <pkrol@simspace.com>
2024-05-07 15:29:07 -0700

    feat: add pageItemCount locale text

--- packages/components/src/PaginationFooter/PaginationFooterLocaleText.ts
+++ packages/components/src/PaginationFooter/PaginationFooterLocaleText.ts
@@ -2,6 +2,11 @@ export interface PaginationFooterLocaleText {
   selectedLabel: (n: number) => string
   pageSizeSelectLabel: string
   itemsShownLabel: string
+  pageItemCount: (
+    itemCountStart: number,
+    itemCountEnd: number,
+    totalItemCount: number,
+  ) => string
 }
 
 export const PAGINATION_FOOTER_DEFAULT_LOCALE_TEXT: PaginationFooterLocaleText =
@@ -9,4 +14,6 @@ export const PAGINATION_FOOTER_DEFAULT_LOCALE_TEXT: PaginationFooterLocaleText =
     selectedLabel: n => `${n} selected`,
     pageSizeSelectLabel: 'Showing',
     itemsShownLabel: 'Showing',
+    pageItemCount: (itemCountStart, itemCountEnd, totalItemCount) =>
+      `${itemCountStart}-${itemCountEnd} of ${totalItemCount}`,
   }
