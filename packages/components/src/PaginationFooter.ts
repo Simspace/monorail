55cc8c540451cf37336ea20acf0bf00a822b5159
Peter Krol <pkrol@simspace.com>
2024-05-07 15:33:42 -0700

    fix: file casing

--- packages/components/src/PaginationFooter.ts
+++ packages/components/src/PaginationFooter.ts
@@ -1,7 +1,7 @@
 // codegen:start { preset: barrel, include: ./PaginationFooter/*.ts?(x) }
 export * from './PaginationFooter/PaginationFooter.js'
-export * from './PaginationFooter/paginationFooterClasses.js'
+export * from './PaginationFooter/PaginationFooterClasses.js'
 export * from './PaginationFooter/PaginationFooterLocaleText.js'
-export * from './PaginationFooter/paginationFooterProps.js'
+export * from './PaginationFooter/PaginationFooterProps.js'
 export * from './PaginationFooter/themeAugmentation.js'
 // codegen:end
