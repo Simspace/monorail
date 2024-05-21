ce114679184cfbee06eac3568a85a1f482642323
Nick Brinser <nickbrinser@gmail.com>
2024-05-21 11:02:46 -0500

    (CFP-2730): add theme overrides to components

--- packages/themes/src/meteor/components/InputBase/themeOverrides.ts
+++ packages/themes/src/meteor/components/InputBase/themeOverrides.ts
@@ -7,7 +7,7 @@ export const MonorailInputBaseOverrides: Components<Theme>['MuiInputBase'] = {
     root: ({ ownerState: { color = 'primary', size = 'medium' }, theme }) => {
       return {
         [`&.${inputBaseClasses.focused}`]: {
-          boxShadow: `0 0 0 3px ${theme.palette[color].focusRing.outer}`,
+          boxShadow: `0 0 0 2px ${theme.palette[color].focusRing.outer}`,
           [`& > fieldset.${outlinedInputClasses.notchedOutline}`]: {
             borderColor: `${theme.palette.default.border.main}`,
             borderWidth: '1px',
@@ -31,9 +31,15 @@ export const MonorailInputBaseOverrides: Components<Theme>['MuiInputBase'] = {
     }),
     error: ({ theme }) => ({
       [`&.${inputBaseClasses.focused}`]: {
-        boxShadow: `0 0 0 3px ${theme.palette.error.focusRing.outer}`,
+        boxShadow: `0 0 0 2px ${theme.palette.error.border.main}`,
         [`& > fieldset.${outlinedInputClasses.notchedOutline}`]: {
-          borderColor: `${theme.palette.error.focusRing.inner}`,
+          borderColor: `${theme.palette.error.border.main}`,
+          borderWidth: '1px',
+        },
+      },
+      [`&:hover`]: {
+        [`& > fieldset.${outlinedInputClasses.notchedOutline}`]: {
+          borderColor: `${theme.palette.error.border.dark}`,
           borderWidth: '1px',
         },
       },
