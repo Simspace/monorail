76581e052064874342eb2321c87190c23dc4df1e
Concourse CI <concourseci@simspace.com>
2024-06-19 16:29:59 +0000

    ci: pages

---
+++ uxe-111-feat/draggable-listitem-story/storybook/sb-addons/storybook-10/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var i=__STORYBOOKADDONS__,{addons:n,types:p,mockChannel:l}=__STORYBOOKADDONS__;n.setConfig({showPanel:!0,panelPosition:"bottom",toolbar:{"storybook/background":{hidden:!0}}});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
