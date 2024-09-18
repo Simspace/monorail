f979e74579c87b24083f9522f948418ef03d40f3
Oscar Pulido <oscar.pulido@tvc.simspace.com>
2024-09-18 08:43:56 -0500

    refactor: removed not needed variable encapsulation

--- packages/themes/src/classic/components/PickersLayout/themeOverrides.ts
+++ packages/themes/src/classic/components/PickersLayout/themeOverrides.ts
@@ -11,73 +11,70 @@ export const MonorailPickersLayoutOverrides: Components<Theme>['MuiPickersLayout
   {
     defaultProps: {},
     styleOverrides: {
-      root: ({ theme }) => {
-        const BORDER_RADIUS = theme.shape.borderRadius
-        return {
-          '&': {
-            padding: theme.spacing(4),
+      root: ({ theme }) => ({
+        '&': {
+          padding: theme.spacing(4),
+        },
+        [`& .${pickersLayoutClasses.actionBar}`]: {
+          padding: 0,
+          height: 'auto',
+        },
+        [`& .${dividerClasses.root}`]: {
+          display: 'none',
+        },
+        [`& .${multiSectionDigitalClockSectionClasses.root}`]: {
+          width: 'auto',
+          padding: theme.spacing(1),
+        },
+
+        /** Calendar container */
+        [`& .${dateCalendarClasses.root}`]: {
+          width: theme.spacing(58),
+          padding: 0,
+
+          [`& .${dateCalendarClasses.viewTransitionContainer}`]: {
+            width: theme.spacing(56),
           },
-          [`& .${pickersLayoutClasses.actionBar}`]: {
+
+          /** Calendar header */
+          [`& .${pickersCalendarHeaderClasses.root}`]: {
             padding: 0,
-            height: 'auto',
-          },
-          [`& .${dividerClasses.root}`]: {
-            display: 'none',
-          },
-          [`& .${multiSectionDigitalClockSectionClasses.root}`]: {
-            width: 'auto',
-            padding: theme.spacing(1),
-          },
+            marginBottom: theme.spacing(4),
+            marginTop: 0,
 
-          /** Calendar container */
-          [`& .${dateCalendarClasses.root}`]: {
-            width: theme.spacing(58),
-            padding: 0,
-
-            [`& .${dateCalendarClasses.viewTransitionContainer}`]: {
-              width: theme.spacing(56),
+            [`& .${pickersCalendarHeaderClasses.label}`]: {
+              whiteSpace: 'nowrap',
             },
+          },
+        },
 
-            /** Calendar header */
-            [`& .${pickersCalendarHeaderClasses.root}`]: {
-              padding: 0,
-              marginBottom: theme.spacing(4),
-              marginTop: 0,
+        /** Days styles */
+        [`& .${pickersDayClasses.root}`]: {
+          height: theme.spacing(8),
+          width: theme.spacing(8),
+          margin: 0,
+          borderRadius: theme.shape.borderRadius,
 
-              [`& .${pickersCalendarHeaderClasses.label}`]: {
-                whiteSpace: 'nowrap',
-              },
+          /** Selected day */
+          [`&.${pickersDayClasses.selected}`]: {
+            backgroundColor: theme.palette.secondary.light,
+            color: theme.palette.default.dark,
+            ['&:hover']: {
+              backgroundColor: theme.palette.secondary.main,
+            },
+            ['&:focus']: {
+              backgroundColor: theme.palette.secondary.main,
             },
           },
 
-          /** Days styles */
-          [`& .${pickersDayClasses.root}`]: {
-            height: theme.spacing(8),
-            width: theme.spacing(8),
-            margin: 0,
-            borderRadius: BORDER_RADIUS,
-
-            /** Selected day */
-            [`&.${pickersDayClasses.selected}`]: {
-              backgroundColor: theme.palette.secondary.light,
-              color: theme.palette.default.dark,
-              ['&:hover']: {
-                backgroundColor: theme.palette.secondary.main,
-              },
-              ['&:focus']: {
-                backgroundColor: theme.palette.secondary.main,
-              },
-            },
-
-            /** Disabled days */
-            [`&.${pickersDayClasses.disabled}`]: {
-              color: theme.palette.primary.dark,
-              ['&:hover']: {
-                backgroundColor: theme.palette.action.hover,
-              },
+          /** Disabled days */
+          [`&.${pickersDayClasses.disabled}`]: {
+            color: theme.palette.primary.dark,
+            ['&:hover']: {
+              backgroundColor: theme.palette.action.hover,
             },
           },
-        }
-      },
+        },
+      }),
     },
   }
