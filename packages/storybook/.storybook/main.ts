9404115a7b76ad550a80d0ec9720ee99ff32017f
Peter Krol <peter@peterkrol.com>
2024-12-11 19:47:36 -0800

    fix: alias jsx-runtime in storybook build

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,10 +1,16 @@
-import { PropItem } from 'react-docgen-typescript';
-import path from 'path';
-const ARIA_PROP_REGEX = /aria.*/;
-const ON_PROP_REGEX = /on.*/;
+import { PropItem } from 'react-docgen-typescript'
+import path from 'path'
+const ARIA_PROP_REGEX = /aria.*/
+const ON_PROP_REGEX = /on.*/
+
 export default {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', 'creevey'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    'creevey',
+  ],
   typescript: {
     // Generate docs/control table on-the-fly using react-docgen-typescript
     // Note: this is an alternative to using the scripted generation of .meta.json files. The desire was to not have to
@@ -14,15 +20,17 @@ export default {
     reactDocgenTypescriptOptions: {
       compilerOptions: {
         allowSyntheticDefaultImports: false,
-        esModuleInterop: false
+        esModuleInterop: false,
       },
       propFilter: (prop: PropItem) => {
-        if ([ARIA_PROP_REGEX, ON_PROP_REGEX].find(regex => regex.test(prop.name))) {
-          return false;
+        if (
+          [ARIA_PROP_REGEX, ON_PROP_REGEX].find(regex => regex.test(prop.name))
+        ) {
+          return false
         }
-        return true;
-      }
-    }
+        return true
+      },
+    },
   },
   webpackFinal: (config: any) => {
     return {
@@ -31,27 +39,41 @@ export default {
         ...config.resolve,
         symlinks: true,
         extensionAlias: {
-          '.js': ['.ts', '.tsx', '.js']
+          '.js': ['.ts', '.tsx', '.js'],
         },
         alias: {
-          '@monorail/components$': path.resolve(__dirname, '../../components/src/index.ts'),
-          '@monorail/components': path.resolve(__dirname, '../../components/src'),
-          '@monorail/themes$': path.resolve(__dirname, '../../themes/src/index.ts'),
+          '@monorail/components$': path.resolve(
+            __dirname,
+            '../../components/src/index.ts',
+          ),
+          '@monorail/components': path.resolve(
+            __dirname,
+            '../../components/src',
+          ),
+          '@monorail/themes$': path.resolve(
+            __dirname,
+            '../../themes/src/index.ts',
+          ),
           '@monorail/themes': path.resolve(__dirname, '../../themes/src'),
-          '@monorail/utils$': path.resolve(__dirname, '../../utils/src/index.ts'),
-          '@monorail/utils': path.resolve(__dirname, '../../utils/src')
+          '@monorail/utils$': path.resolve(
+            __dirname,
+            '../../utils/src/index.ts',
+          ),
+          '@monorail/utils': path.resolve(__dirname, '../../utils/src'),
+          'react/jsx-runtime.js': 'react/jsx-runtime',
+          'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
         },
         fallback: {
-          assert: require.resolve('assert')
-        }
-      }
-    };
+          assert: require.resolve('assert'),
+        },
+      },
+    }
   },
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
-};
+    autodocs: true,
+  },
+}
