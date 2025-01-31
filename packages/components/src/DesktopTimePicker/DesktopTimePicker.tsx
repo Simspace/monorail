3293aaf3ef994635103e6edbde8aea433661ccad
Oscar Pulido <oscar.pulido@tvc.simspace.com>
2025-01-31 10:42:15 -0500

    feat: new styles for DesktopTimerPicker

--- packages/components/src/DesktopTimePicker/DesktopTimePicker.tsx
+++ packages/components/src/DesktopTimePicker/DesktopTimePicker.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import type { DesktopTimePickerProps as MuiDesktopTimePickerProps } from '@mui/x-date-pickers'
 import { DesktopTimePicker as MuiDesktopTimePicker } from '@mui/x-date-pickers'
 
@@ -11,8 +12,23 @@ import { DesktopTimePicker as MuiDesktopTimePicker } from '@mui/x-date-pickers'
  *
  * - [DesktopTimePicker API](https://mui.com/x/api/date-pickers/desktop-time-picker/)
  */
-export const DesktopTimePicker: typeof MuiDesktopTimePicker =
-  MuiDesktopTimePicker
+export const DesktopTimePicker: typeof MuiDesktopTimePicker = React.forwardRef(
+  (props, ref) => (
+    <MuiDesktopTimePicker
+      ref={ref}
+      slots={{ actionBar: () => null }}
+      /**
+       * The reason for this is here instead of in
+       * packages/themes/src/classic/components/PickersLayout/themeOverrides.ts
+       * is because the layout padding depends on whether the component is wrapping a DateTimePicker or a
+       * DesktopDateTimePicker. The padding is different for each.
+       */
+      slotProps={{ layout: { sx: { paddingX: 2, pt: 4, paddingBottom: 6 } } }}
+      {...props}
+      closeOnSelect={false}
+    />
+  ),
+) as typeof MuiDesktopTimePicker
 
 export interface DesktopTimePickerProps<TDate = Date>
   extends MuiDesktopTimePickerProps<TDate> {}
