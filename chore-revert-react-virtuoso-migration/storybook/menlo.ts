05a83b3411bb4411685241a4115f2ce325aec12f
concourse ci <concourseci@simspace.com>
2025-05-30 22:08:49 +0000

    ci: pages

---
+++ chore-revert-react-virtuoso-migration/storybook/menlo.ts
@@ -0,0 +1,10 @@
+import MenloRegular from './Menlo/Menlo-Regular.woff2'
+
+export const menlo = `
+  @font-face {
+    font-family: "Menlo Regular";
+    font-style: normal;
+    font-weight: normal;
+    src: url("${MenloRegular}") format('woff2');
+  }
+`
